5c256bd40273dfcb42c00338e280f8be371750bc
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-12 21:10:55 +0300

    scriipt

---
+++ studentai/violetaVinogrodska/lesson-1/exercise-1/script.js
@@ -0,0 +1,14 @@
+const diena = 12
+const veikiaVscode = false
+const vyriskiVardai = ["Tomas", "Paulius", "Donatas"]
+const studentas = {
+             vardas: "Violeta"
+             pavarde: "Vino"
+}
+
+
+console.log(manoVardas)
+console.log(diena)
+console.log(veikiaVscode)
+console.log(vyriskiVardai)
+console.log(studentas)

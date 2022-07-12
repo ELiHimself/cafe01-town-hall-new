149ab687503f2b920b1b42f52a31f650928c578a
Panucio31 <m.januska1990@gmail.com>
2022-07-12 21:15:51 +0300

    indentations fixed

--- studentai/mantasJanuska/lesson-1/exercise-1/script.js
+++ studentai/mantasJanuska/lesson-1/exercise-1/script.js
@@ -1,17 +1,23 @@
 const name = "mantas"
-  console.log(name);
+  
 
 const age = 132
-  console.log(age);
+ 
 
 const isBald=true
-    console.log(isBald);
+  
 
 const dog = ["age", "breed", "wight"]
-    console.log(dog)
+  
 
 let person = {
-    firstName: "John",
-    age: 56,
-    height: 178,
+  firstName: "John",
+  age: 56,
+  height: 178,
 }
+
+console.log(name)
+console.log(age)
+console.log(isBald)
+console.log(dog)
+console.log(person)

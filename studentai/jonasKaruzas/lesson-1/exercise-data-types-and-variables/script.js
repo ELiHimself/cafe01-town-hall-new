f905115b5f12de719c850e8a5a714c700d8849f0
JonasKaruzas <jonas.karuzas@gmail.com>
2022-07-12 20:37:41 +0300

    object fix

--- studentai/jonasKaruzas/lesson-1/exercise-data-types-and-variables/script.js
+++ studentai/jonasKaruzas/lesson-1/exercise-data-types-and-variables/script.js
@@ -2,7 +2,7 @@ const myName = "Jonas";
 const age = 69;
 const isAwake = true;
 const bestDays = ["Monday", "Tuesday", "Friday"];
-const person = { name: name, age: age };
+const person = { name: myName, age: age };
 
 console.log(myName);
 console.log(age);

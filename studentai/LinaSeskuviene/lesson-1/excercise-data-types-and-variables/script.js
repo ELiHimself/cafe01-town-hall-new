b3d506d510fc9a3e072ccfdb54a0054a8365cfff
LinaSeskuviene <ses.lina@gmail.com>
2022-07-12 21:21:41 +0300

    java first excercise

---
+++ studentai/LinaSeskuviene/lesson-1/excercise-data-types-and-variables/script.js
@@ -0,0 +1,17 @@
+const name = "Daiva"
+  console.log(name);
+
+const age = 30
+  console.log(age);
+
+const text = ['suo', 'kate', 'vezlys']
+ console.log(text);
+ console.log(text[0]);
+  
+const a = 5
+const b = 2
+ console.log(a+b);
+ console.log(a>b);
+
+ const dog = {age: "5", color: "black"}
+ console.log(dog);

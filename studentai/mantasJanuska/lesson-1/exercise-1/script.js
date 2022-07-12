f7c5aaeed839849bce66d064546d610f407499ae
Panucio31 <m.januska1990@gmail.com>
2022-07-12 20:47:25 +0300

    data types

---
+++ studentai/mantasJanuska/lesson-1/exercise-1/script.js
@@ -0,0 +1,17 @@
+const name = "mantas"
+  console.log(name);
+
+const age = 132
+  console.log(age);
+
+const isBald=true
+    console.log(isBald);
+
+const dog = ["age", "breed", "wight"]
+    console.log(dog)
+
+let person = {
+    firstName: "John",
+    age: 56,
+    height: 178,
+}

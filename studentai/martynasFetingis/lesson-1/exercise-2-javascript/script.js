a12b62c2085fb1eacb9ec7f4f86f0adbdcf53544
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-07-12 20:42:21 +0300

    2nd exercise

---
+++ studentai/martynasFetingis/lesson-1/exercise-2-javascript/script.js
@@ -0,0 +1,20 @@
+const name = "martynas";
+console.log(name);
+
+const surname = "fetingis";
+console.log(surname);
+
+const age = 29;
+console.log(age);
+
+const isHappy = false;
+console.log(isHappy);
+
+const activieties = ["sport", "gaming", "fishing"];
+console.log(activieties);
+
+const loc = {
+  born: "Taurage",
+  currently: "Vilnius",
+};
+console.log(loc);

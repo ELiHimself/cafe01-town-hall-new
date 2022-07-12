279d36f05ab2104f7b29bc50fca15c2096d6221e
renaldas112 <renaldas112@gmail.com>
2022-07-12 20:26:51 +0300

    done

---
+++ studentai/renaldasBarzdaitis/lesson-1/exercise-data-types-and-variables/script.js
@@ -0,0 +1,17 @@
+const name = "Hellen"
+console.log(name);
+
+const age = 32
+console.log(age);
+
+const booleanTrue = true
+console.log(booleanTrue);
+
+const booleanFalse = false
+console.log(booleanFalse);
+
+const array = [1, "vienas", 2, "du", 3, "trys"]
+console.log(array);
+
+const object = {name: "renaldas", age: 22, knowsJS: false}
+console.log(object);

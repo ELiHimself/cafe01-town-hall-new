538ac954f412796c2c2b06c9233332ebf89463cb
Vilius Garba <vilius.garba@gmail.com>
2022-07-12 20:32:18 +0300

    exercise

---
+++ studentai/viliusGarba/lesson-1/exercise-1/script.js
@@ -0,0 +1,19 @@
+const name = "william"
+console.log(name);
+
+const age = 27
+console.log(age);
+
+const isAbscent = false
+console.log(isAbscent);
+
+const colors = ["red", "blue", "green", "yellow", "black"]
+console.log(colors);
+
+const person = {
+    firstName: "Vilius",
+    lastName: "Garba",
+    age: 27,
+    favouriteColor: "black",
+}
+console.log(person);

41ebdbdf9d002dff628a45ce593fe57cc89572f5
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-07-13 18:59:02 +0300

    javascripting 10/20

--- studentai/martynasFetingis/lesson-1/exercise-2-javascripting/javascripting/if-statement.js
+++ studentai/martynasFetingis/lesson-1/exercise-2-javascripting/javascripting/if-statement.js
@@ -1,6 +1,6 @@
 const fruit = "orange";
 if (fruit.length > 5) {
-  console.log("the fruit name is longer than 5 characters.");
+  console.log("The fruit name has more than five characters.");
 } else {
-  console.log("The fruit name is has five characters or less.");
+  console.log("The fruit name has five characters or less.");
 }

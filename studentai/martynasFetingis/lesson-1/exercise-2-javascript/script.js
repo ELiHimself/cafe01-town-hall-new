db113a3c080507aef32f65be4de3e9ebdae64f08
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-07-13 18:33:28 +0300

    branch fix

---
+++ studentai/martynasFetingis/lesson-1/exercise-2-javascript/script.js
@@ -0,0 +1,16 @@
+const name = "martynas";
+const surname = "fetingis";
+const age = 29;
+const isHappy = false;
+const activieties = ["sport", "gaming", "fishing"];
+const loc = {
+    born: "Taurage",
+    currently: "Vilnius",
+  };
+  
+console.log(name);
+console.log(surname);
+console.log(age);
+console.log(isHappy);
+console.log(activieties);
+console.log(loc);

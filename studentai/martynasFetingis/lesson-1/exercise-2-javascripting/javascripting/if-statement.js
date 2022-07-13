dd0d85d10b231c8b1986c7d0d046a82224246feb
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-07-13 18:47:54 +0300

    Fixed forlder names & changed var<->const

---
+++ studentai/martynasFetingis/lesson-1/exercise-2-javascripting/javascripting/if-statement.js
@@ -0,0 +1,6 @@
+const fruit = "orange";
+if (fruit.length > 5) {
+  console.log("the fruit name is longer than 5 characters.");
+} else {
+  console.log("The fruit name is has five characters or less.");
+}

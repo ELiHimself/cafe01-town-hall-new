4512c00db0890b0bec286bbcad0b30c0d9ef259b
titasas <titas.petrauskas@gmail.com>
2022-07-13 21:42:25 +0300

    adjusted date formatting

--- studentai/titasPetrauskas/lesson-2/exercise-functions/script.js
+++ studentai/titasPetrauskas/lesson-2/exercise-functions/script.js
@@ -11,6 +11,7 @@ console.log(greeting);
 function second() {
   console.log('This one logs something but takes no parameters');
 }
+second();
 
 //number three(void function that takes one parameter as object and mutates that object)
 const randomObject = {
@@ -21,15 +22,18 @@ const randomObject = {
 function makeOlder(someObject) {
   someObject.age += 1;
 }
-
-console.log(makeOlder(randomObject));
+makeOlder(randomObject);
 
 //number four(return function that takes no arguments but returns something (e.g. current time)
 function fourth() {
-  return Date();
+  const currentTime = new Date().toLocaleDateString('en-us', { weekday:"long", year:"numeric", month:"short", day:"numeric"}) ;
+  return currentTime;
 }
+console.log(fourth());
 
-//number five(return function that takes 1..n number of arguments, does something with input and returns the result)
+//number fifth(return function that takes 1..n number of arguments, does something with input and returns the result)
 function fifth(a, b, c) {
   return a * b - c;
 }
+
+console.log(fifth(15, 5, 55));

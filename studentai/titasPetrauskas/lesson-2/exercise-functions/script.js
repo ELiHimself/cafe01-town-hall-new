bb2e7ae9d1677a751e9f83c2c21f8b938de8807e
titasas <titas.petrauskas@gmail.com>
2022-07-13 20:55:11 +0300

    challenge one done, not tested

---
+++ studentai/titasPetrauskas/lesson-2/exercise-functions/script.js
@@ -0,0 +1,35 @@
+// CHALLENGE ONE
+// number one
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+const greeting = greet('awesome soon to be front-enders')
+console.log(greeting);
+
+//number two(void function that takes no parameters and does something)
+function second() {
+  console.log('This one logs something but takes no parameters');
+}
+
+//number three(void function that takes one parameter as object and mutates that object)
+const randomObject = {
+  name: 'Vardenis',
+  age: 55,
+}
+
+function makeOlder(someObject) {
+  someObject.age += 1;
+}
+
+console.log(makeOlder(randomObject));
+
+//number four(return function that takes no arguments but returns something (e.g. current time)
+function fourth() {
+  return Date();
+}
+
+//number five(return function that takes 1..n number of arguments, does something with input and returns the result)
+function fifth(a, b, c) {
+  return a * b - c;
+}

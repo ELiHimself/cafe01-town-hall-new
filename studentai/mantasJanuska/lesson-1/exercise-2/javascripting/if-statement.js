681b4ada47e10f1b34c2b5e80bc88701f1d09290
Panucio31 <m.januska1990@gmail.com>
2022-07-13 19:16:58 +0300

    accesing arrays done

--- studentai/mantasJanuska/lesson-1/exercise-2/javascripting/if-statement.js
+++ studentai/mantasJanuska/lesson-1/exercise-2/javascripting/if-statement.js
@@ -1,4 +1,6 @@
-var fruit = "orange"
-if (fruit.lenght > 5) {
+var fruit = "orange";
+if (fruit.length > 5) {
     console.log("The fruit name has more than five characters.")
+} else {
+    console.log("The fruit name has five characters or less.")
 }

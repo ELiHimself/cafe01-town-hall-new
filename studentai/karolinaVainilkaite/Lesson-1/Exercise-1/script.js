38fc7f2e77d4cada6796d2b1f88726b9e981753b
kavaini <karolina.vainilkaite@gmail.com>
2022-07-13 18:37:51 +0200

    pirmas readMe

---
+++ studentai/karolinaVainilkaite/Lesson-1/Exercise-1/script.js
@@ -0,0 +1,12 @@
+const name = "Karolina"
+  console.log(name);
+
+const age = 32
+  console.log(age);
+
+const isAway = true
+    console.log(isAway);
+
+const bestMonths = ["August", "November", "December"]
+
+const person = {name: myName, age: age};

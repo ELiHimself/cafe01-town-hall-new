eac4ca6dc607f2da32a4d36a648139123224fb8c
titasas <titas.petrauskas@gmail.com>
2022-07-14 19:40:33 +0300

    quick fix

--- studentai/titasPetrauskas/lesson-2/class-exercises/8-getGeneration.js
+++ studentai/titasPetrauskas/lesson-2/class-exercises/8-getGeneration.js
@@ -1,39 +1,51 @@
 function getGeneration(number, gender) {
   if (number < 0 || gender === "m") {
     switch (number) {
-      case -1: return 'father';
-      break;
-      case -2: return 'grandfather';
-      break;
-      case -3: return 'great grandfather';
-      break;
+      case -1: 
+        return 'father';
+        break;
+      case -2: 
+        return 'grandfather';
+        break;
+      case -3: 
+        return 'great grandfather';
+        break;
     }
   } else if (number > 0 || gender === "m") {
     switch (number) {
-      case 1: return 'son';
-      break;
-      case 2: return 'grandson';
-      break;
-      case 3: return 'great grandson';
-      break;
+      case 1: 
+        return 'son';
+        break;
+      case 2: 
+        return 'grandson';
+        break;
+      case 3: 
+        return 'great grandson';
+        break;
     }
   } else if (number < 0 || gender === "f") {
     switch (number) {
-      case -1: return 'mother';
-      break;
-      case -2: return 'grandmother';
-      break;
-      case -3: return 'great grandmother';
-      break;
+      case -1: 
+        return 'mother';
+        break;
+      case -2: 
+        return 'grandmother';
+        break;
+      case -3: 
+        return 'great grandmother';
+        break;
     }
   } else if (number > 0 || gender === "f") {
     switch (number) {
-      case 1: return 'daughter';
-      break;
-      case 2: return 'granddaughter';
-      break;
-      case 3: return 'great granddaughter';
-      break;
+      case 1: 
+        return 'daughter';
+        break;
+      case 2: 
+        return 'granddaughter';
+        break;
+      case 3: 
+        return 'great granddaughter';
+        break;
     }
   }
 }

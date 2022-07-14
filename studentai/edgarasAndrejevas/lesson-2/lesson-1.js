8563e7c49ba9ec0d602e00be1e6c821eeaf51090
EdgarasAnd <eandrejevas@yahoo.com>
2022-07-14 20:02:24 +0300

    Trying to make lesson 2 work

---
+++ studentai/edgarasAndrejevas/lesson-2/lesson-1.js
@@ -0,0 +1,23 @@
+const age = prompt('What is your age?');
+
+function checkAge(age) {
+    if (age > 18) {
+        return true;
+    } else {
+        /// ...
+        return confirm ('Did parents allow you?');
+    }
+}
+
+alert(checkAge)
+
+// let age = prompt('What is your age?');
+
+// function checkAge(age) {
+//   if (age > 18) {
+//     return true;
+//   }  
+//   return confirm("Did parents allow you?");
+// }
+
+// alert(checkAge);

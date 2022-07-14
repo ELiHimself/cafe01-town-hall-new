a77a1ca93838f1231e45f972734e9efe86f9cbcb
Mantas <mantaspauza@gmail.com>
2022-07-14 21:05:33 +0300

    done with lesson-2 exercise-functions

--- studentai/mantasPauza/lesson-2/exercise-functions/script.js
+++ studentai/mantasPauza/lesson-2/exercise-functions/script.js
@@ -31,6 +31,8 @@ function mutate(person) {
     return person;
 }
 
+console.log(person);
+
 mutate(person);
 
 console.log(person);
@@ -41,3 +43,45 @@ const currentTime = new Date();
 console.log(currentTime);
 
 // *************************
+
+function getAmountOfLetters(arguments) {
+    return arguments.length;
+}
+
+console.log(getAmountOfLetters('kebabai' + ' yra ' + 'skanu'));
+
+// **********************************************************************************
+
+const cars = ['audi', 'bmw', 'mercedes', 'opel'];
+
+console.log(cars.map(car => car.charAt(0).toUpperCase()+car.slice(1)));
+
+// *************************
+
+const emptyArrow = console.log('I am a void function');
+
+// *************************
+
+const mutation = person => {
+    person.age = 29;
+    person.name = 'Mantas Pauza';
+    person.hobbies.push('sleeping');
+    return person;
+}
+
+mutation(person);
+
+console.log(person);
+
+// *************************    
+
+const currentTimeNew = () => new Date().toLocaleTimeString();
+
+const results = currentTimeNew();
+console.log(results);
+
+// *************************
+
+const getAmountOfLettersB = (arguments) => arguments.length;
+
+console.log(getAmountOfLettersB('kebabai' + ' yra ' + 'labai ' + 'skanu'));

68c13f184885fd7a2050687193ed9ce2788b5fd9
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-14 18:53:09 +0300

    exercise-2-done

---
+++ studentai/violetaVinogrodska/lesson-2/class-exercises/1-grazinamaVerte.js
@@ -0,0 +1,20 @@
+function checkAge(age) {
+    if (age > 18) {
+        return true;
+    } else {
+        return confirm("Did parents allow you?");
+    }
+};
+
+function checkAge2(age) {
+    if (age > 18) {
+        return true;
+    }
+    return confirm("Did parents allow you?");
+};
+
+console.log(checkAge(19));
+console.log(checkAge(2));
+
+console.log(checkAge2(19));
+console.log(checkAge2(2));

1bd90869ca6f769d5740a5ff461fdb7312cf9211
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-14 20:43:33 +0300

    kabliataskis

--- studentai/violetaVinogrodska/lesson-2/class-exercises/3-funkcijaMin.js
+++ studentai/violetaVinogrodska/lesson-2/class-exercises/3-funkcijaMin.js
@@ -1,10 +1,10 @@
 function getMin(array) {
-    const minimum = array.sort((a, b) => a - b)
+    const minimum = array.sort((a, b) => a - b);
 
     return {
         min: minimum[0],
-    }
-}
+    };
+};
 console.log(getMin([5, 6]));
 console.log(getMin([10,4]));
 console.log(getMin([3,-1]));

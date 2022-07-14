8f51019194d7607df10df6251e49be1ee46ce112
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-14 21:48:39 +0300

    arciautiesos

--- studentai/violetaVinogrodska/lesson-2/class-exercises/4-funkcijaCalculate.js
+++ studentai/violetaVinogrodska/lesson-2/class-exercises/4-funkcijaCalculate.js
@@ -1,3 +1,8 @@
-function calculate(num1, num2) {
-    
-}
+function calculate(num1, num2, action) {
+    return action === "composition" ? num1 + num2 
+        : action === "substraction" ? num1 - num2
+        : action === "multiplication" ? num1 * num2
+        : action === "division" ? num1 / num2;
+};
+
+console.log(calculate(5, 5, "multiplication"));

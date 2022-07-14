e597604ae03878e148b314200b965c3dea6c7ab5
renaldas112 <renaldas112@gmail.com>
2022-07-14 19:31:40 +0300

    bandau daryt

---
+++ studentai/renaldasBarzdaitis/lesson-functions/script.js
@@ -0,0 +1,23 @@
+// uzduotis 2
+
+// const age = 19;
+
+// function checkAge(){
+//     return (age >=18 ? true : false);
+// }
+
+// console.log(checkAge());
+
+// uzduotis 3
+
+function composition(a, b) {
+    return (a + b);
+}
+console.log(composition(15, 5));
+
+function substract(a, b) {
+    return (a - b);
+}
+console.log(substract(15, 5));
+// uzduotis 4
+

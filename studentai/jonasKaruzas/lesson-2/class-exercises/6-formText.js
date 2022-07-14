39b9e19ac67fde7e676ee4cdeb9e8667d432c9a9
JonasKaruzas <60874740+JonasKaruzas@users.noreply.github.com>
2022-07-14 19:57:07 +0300

    Jonas Karuzas - minor fix and IF to SWITCH (#58)
    * test2
    
    * test2
    
    * Javascripting all 20 done
    
    * dar
    
    * panaudotas map
    
    * reduce test
    
    * lesson2 - ex1
    
    * 1-3 uzduotys
    
    * 4 uzduotis
    
    * 5 uzduotis
    
    * 6 uzduotis
    
    * 7 uzduotis
    
    * 8 uzduotis
    
    * 3 uzduotis su arrow function
    
    * minor fix'es
    
    * Update studentai/jonasKaruzas/lesson-2/exercise-functions/script.js
    
    * 4 uzduotis - from if to switch
    
    * 6 uzduotis - prettier magic
    
    Co-authored-by: Mindaugas Paskevicius <pask.mindaugas@gmail.com>

--- studentai/jonasKaruzas/lesson-2/class-exercises/6-formText.js
+++ studentai/jonasKaruzas/lesson-2/class-exercises/6-formText.js
@@ -1,11 +1,11 @@
 function formText(string, number) {
-    return string.
-        split('').
-        splice(0, string.length - number).
-        join('');
+  return string
+    .split('')
+    .splice(0, string.length - number)
+    .join('');
 }
 
-// Ar teisingas toks formatavimas? (man vizualiai patogesnis) 
+// Ar teisingas toks formatavimas? (man vizualiai patogesnis)
 // Nes originaliai viska vienoje eiluteje dariau.
 
 console.log(formText('Hello World', 3));

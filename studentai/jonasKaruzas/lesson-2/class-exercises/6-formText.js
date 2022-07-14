3b7ee551947720a9de26641b782e3fc5725321a8
Violeta Vinogrodska <105384019+Wiolla55525@users.noreply.github.com>
2022-07-14 19:42:33 +0300

    example (#52) (#57)
    * example (#52)
    
    * Jonas Karuzas - lesson 2 - ex1 (#51)
    
    * test2
    
    * test2
    
    * Javascripting all 20 done
    
    * dar
    
    * panaudotas map
    
    * reduce test
    
    * lesson2 - ex1
    
    * 1-3 uzduotys
    
    * 4 uzduotis
    
    * 5 uzduotis
    
    * 6 uzduotis
    
    * 7 uzduotis
    
    * 8 uzduotis
    
    * 3 uzduotis su arrow function
    
    * minor fix'es
    
    * Update studentai/jonasKaruzas/lesson-2/exercise-functions/script.js
    
    Co-authored-by: Mindaugas Paskevicius <pask.mindaugas@gmail.com>
    
    Co-authored-by: Mindaugas Paskevicius <pask.mindaugas@gmail.com>
    Co-authored-by: JonasKaruzas <60874740+JonasKaruzas@users.noreply.github.com>

---
+++ studentai/jonasKaruzas/lesson-2/class-exercises/6-formText.js
@@ -0,0 +1,11 @@
+function formText(string, number) {
+    return string.
+        split('').
+        splice(0, string.length - number).
+        join('');
+}
+
+// Ar teisingas toks formatavimas? (man vizualiai patogesnis) 
+// Nes originaliai viska vienoje eiluteje dariau.
+
+console.log(formText('Hello World', 3));

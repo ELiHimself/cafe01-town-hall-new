af3e8e7c51c6be31df843d37e08de86046342502
Panucio31 <m.januska1990@gmail.com>
2022-07-15 14:30:37 +0300

    exercise 8 finished. let's cath some pokemon.

--- studentai/mantasJanuska/lesson-2/class-exercises/8-proteviaiPalikuoniai.js
+++ studentai/mantasJanuska/lesson-2/class-exercises/8-proteviaiPalikuoniai.js
@@ -1,3 +1,31 @@
 function getGeneration(num, gender) {
-    
+    if (num === 0 && gender === 'm'){
+        return "it's a me Mario";
+    }else if (num === 1 && gender === 'm'){
+        return "Welcome my son!";
+    }else if (num === 2 && gender === 'm'){
+        return "Im too young to have a grandson";
+    }else if (num === 3 && gender === 'm'){
+        return "Grandson you are great";
+    }else if (num === -1 && gender === 'm'){
+        return "No, i am your father";
+    }else if (num === -2 && gender === 'm'){
+        return "Hello grandfather";
+    }else if (num === -3 && gender === 'm'){
+        return "Grand grandfather";
+    }else if (num === 0 && gender === 'f'){
+        return "it's a me Princes Peach";
+    }else if (num === 1 && gender === 'f'){
+        return "Welcome my daughter!";
+    }else if (num === 2 && gender === 'f'){
+        return "Im too young to have a granddauther";
+    }else if (num === 3 && gender === 'f'){
+        return "Granddauhter, you are great";
+    }else if (num === -1 && gender === 'f'){
+        return "dont talk with your mother like that";
+    }else if (num === -2 && gender === 'f'){
+        return "Hello grandmother";
+    }else if (num === -3 && gender === 'f'){}
+        return "Grand grandmother is grand";
 }
+console.log(getGeneration(3, "f"));

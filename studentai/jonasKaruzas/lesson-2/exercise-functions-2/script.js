45c58ac142d27b331d7511fc0c3e513c57b0c00c
JonasKaruzas <jonas.karuzas@gmail.com>
2022-07-17 23:39:22 +0300

    creating cards, but problem with weaknesses

--- studentai/jonasKaruzas/lesson-2/exercise-functions-2/script.js
+++ studentai/jonasKaruzas/lesson-2/exercise-functions-2/script.js
@@ -2,7 +2,6 @@ const onlyFirstEvolution = true;
 
 function printPokemon(pokemonItem) {
   let result = '';
-
   const pokemonId = pokemonItem.id;
   const pokemonName = pokemonItem.name;
   const pokemonWeaknessesLength = pokemonItem.weaknesses.length;
@@ -33,6 +32,7 @@ function printPokemon(pokemonItem) {
     result += 'However, it has no available evolutions.';
   }
 
+  showPokemon(pokemonItem, result);
   console.log(result);
 }
 
@@ -53,12 +53,61 @@ function printPokemons(pokemonList, printCount, offset = 0) {
 
   selected.forEach((element) => {
     printPokemon(element);
-    showPokemon(element);
   });
 }
 
-function showPokemon(pokemonItem) {}
+function showPokemon(pokemonItem, result) {
+  const card = document.createElement('div');
+  const card_img = document.createElement('img');
+  const card_name = document.createElement('div');
+  const card_description = document.createElement('div');
 
-printPokemons(pokemons, 5, 0);
+  card.classList.add('card');
+  card_img.classList.add('card__img');
+  card_name.classList.add('card__name');
+  card_description.classList.add('card__description');
+
+  card_description.innerText = result;
+  card_img.src = pokemonItem.img;
+  card_name.innerText = pokemonItem.name;
+
+  card.appendChild(card_img);
+  card.appendChild(card_name);
+  card.appendChild(card_description);
+
+  document.querySelector('main').appendChild(card);
+}
+
+document.querySelectorAll('input').forEach((el) => {
+  el.addEventListener('change', getInputNumbersAndShow);
+});
+
+let startFromNr = 0;
+let printCountNr = 0;
+
+function getInputNumbersAndShow(target) {
+  const id = target.srcElement.id;
+  const value = target.srcElement.value;
+
+  if (id === 'startFromNr') {
+    startFromNr = value;
+  }
+
+  if (id === 'printCountNr') {
+    printCountNr = value;
+  }
+
+  deleteAllCards();
+  printPokemons(pokemons, printCountNr, startFromNr);
+}
+
+function deleteAllCards() {
+  const main = document.querySelector('main');
+  while (main.lastChild) {
+    main.lastChild.remove();
+  }
+}
+
+// printPokemons(pokemons, 50, 0);
 console.log('+++++');
-printPokemons(pokemons, 5, 3);
+// printPokemons(pokemons, 5, 3);

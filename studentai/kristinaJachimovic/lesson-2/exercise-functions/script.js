ae4810d9e3928dafac0f7ef4bc95651dc8a55919
Kristina <kristtule@gmail.com>
2022-07-17 12:36:25 +0300

    some hw

--- studentai/kristinaJachimovic/lesson-2/exercise-functions/script.js
+++ studentai/kristinaJachimovic/lesson-2/exercise-functions/script.js
@@ -1,8 +1,22 @@
 const currentTime = new Date();
 
 function esam() {
-  return `Dabar yra ${currentTime.getFullYear()} metai`
+  return `Dabar yra ${currentTime.getFullYear()} metai`;
 };
 console.log(esam());
 
 console.log(new Intl.DateTimeFormat('en-US').format(currentTime));
+
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+console.log(greet('world'));
+
+const greeting = greet('Awesome soon to be front enders');
+console.log(greeting);
+
+function a() {
+  console.log('lol');
+}
+a();
+

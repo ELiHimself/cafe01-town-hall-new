a0d6597b1590b460e18ef697f849511dee704eb3
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-07-18 20:24:18 +0300

    2nd commit

--- studentai/martynasFetingis/lesson-3/aritmetic-operators/script.js
+++ studentai/martynasFetingis/lesson-3/aritmetic-operators/script.js
@@ -18,3 +18,37 @@ function multi(a, b) {
   return a * b;
 }
 console.log(multi(10, 5));
+
+// 4th function - Exponential //
+
+function expo(a, b) {
+  return a ** b;
+}
+console.log(expo(10, 3));
+
+// 5th function - Division //
+
+function div(a, b) {
+  return a / b;
+}
+console.log(div(10, 5));
+
+// 6th function - Modulus(remainder) //
+
+function mod(a, b) {
+  return a % b;
+}
+console.log(mod(10, 3));
+
+// 7th function - increment //
+
+function increment(a) {
+  return ++a;
+}
+console.log(increment(1));
+
+// 8th function - decrement //
+function decrement(a) {
+  return --a;
+}
+console.log(decrement(10));

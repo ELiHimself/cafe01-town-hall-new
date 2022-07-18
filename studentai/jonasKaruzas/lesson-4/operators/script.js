4c8da11bcf61e2e8f42c74429af045f59ce37604
JonasKaruzas <jonas.karuzas@gmail.com>
2022-07-18 18:39:33 +0300

    lesson 4 - 3 uzduotys

---
+++ studentai/jonasKaruzas/lesson-4/operators/script.js
@@ -0,0 +1,15 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function subtractFromHundred(num1) {
+  return 100 - num1;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+console.log(add(11, 22));
+console.log(subtractFromHundred(98));
+console.log(multiply(2, 5));

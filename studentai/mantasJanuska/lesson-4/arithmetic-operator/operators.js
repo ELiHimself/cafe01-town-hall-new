375af767427875b908d5ed1ed0c105262ce0f4c2
Panucio31 <m.januska1990@gmail.com>
2022-07-18 20:05:14 +0300

    operators done

--- studentai/mantasJanuska/lesson-4/arithmetic-operator/operators.js
+++ studentai/mantasJanuska/lesson-4/arithmetic-operator/operators.js
@@ -15,3 +15,31 @@ function mutilpy(a, b){
 
 }
 console.log(mutilpy(6, 8));
+
+function expo(num){
+    return expo = 20 ** num;
+}
+console.log(expo(3));
+
+function divide(a, b){
+    return divide = a / b;
+}
+console.log(divide(322, 2));
+
+function modulus(a, b){
+    return modulus = a % b;
+}
+console.log(modulus(7, 3));
+
+function increment(num){
+    increment = num++;
+    return num;
+    
+}
+console.log(increment(3));
+
+function decrement(num){
+    decrement = num--;
+    return num;
+}
+console.log(decrement(3));

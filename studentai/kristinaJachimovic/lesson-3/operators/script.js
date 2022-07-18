8b99e0b6bcb71e76ef87b39eb3835cf8193518c4
Kristina <kristtule@gmail.com>
2022-07-18 18:52:18 +0300

    uzduotis done

---
+++ studentai/kristinaJachimovic/lesson-3/operators/script.js
@@ -0,0 +1,14 @@
+function sum( a, b) {
+  console.log(a + b);
+}
+sum(3, 3);
+
+function oneNumber(num) {
+  return 100 - num;
+}
+console.log(oneNumber(5));
+
+function multi(a, b) {
+  return a * b;
+}
+console.log(multi(2, 2));

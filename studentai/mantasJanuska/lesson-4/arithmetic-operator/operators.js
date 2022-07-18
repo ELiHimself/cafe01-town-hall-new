12fa77151ea81f00ce974a1edc1a98546dc4e781
Panucio31 <m.januska1990@gmail.com>
2022-07-18 18:51:33 +0300

    three changes done

---
+++ studentai/mantasJanuska/lesson-4/arithmetic-operator/operators.js
@@ -0,0 +1,17 @@
+function myNum(a, b){
+   return myNum = a + b;
+    
+}
+console.log(myNum(3, 4))
+
+
+function minus(num){
+    return minus = 100 - num;
+}
+console.log(minus(34));
+
+function mutilpy(a, b){
+    return mutilpy = a * b;
+
+}
+console.log(mutilpy(6, 8));

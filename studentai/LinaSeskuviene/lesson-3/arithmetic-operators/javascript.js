8313be36965c74cef3581a26aaacf4d49a467048
LinaSeskuviene <ses.lina@gmail.com>
2022-07-18 18:47:18 +0300

    lesson 3 excercise 1

---
+++ studentai/LinaSeskuviene/lesson-3/arithmetic-operators/javascript.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 5));
+
+function subtract(c) {
+    return 100 - c;
+}
+console.log(subtract(35));
+
+function multiply(d, e) {
+    return d * e;
+}
+console.log(multiply(6, 8));

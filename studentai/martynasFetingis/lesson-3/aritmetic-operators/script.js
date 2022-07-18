7c86594f6e3a73d97f0e5468a2e5dd622705ec2b
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-07-18 18:41:14 +0300

    pirma uzduotis

---
+++ studentai/martynasFetingis/lesson-3/aritmetic-operators/script.js
@@ -0,0 +1,20 @@
+// 1st function - SUM //
+
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(5, 7));
+
+// 2nd function - SUBTRACTION //
+
+function sub(a) {
+  return 100 - a;
+}
+console.log(sub(55));
+
+// 3rd function - MULTIPLICATION //
+
+function multi(a, b) {
+  return a * b;
+}
+console.log(multi(10, 5));

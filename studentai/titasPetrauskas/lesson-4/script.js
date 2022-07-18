a765196e472fda43b66154c9e504c4667030fa07
titasas <titas.petrauskas@gmail.com>
2022-07-18 18:37:29 +0300

    arithmetic operators first three tasks

---
+++ studentai/titasPetrauskas/lesson-4/script.js
@@ -0,0 +1,20 @@
+// 1. Write a function that takes two numbers as arguments, returns their sum AND logs result to the console.
+function sum(a, b) {
+  return console.log(a + b);
+}
+
+sum(20, 30);
+
+//2. Write a function that takes one number as an argument which will subtract from 100, return and console log the result.
+function subtract(a) {
+  return console.log(100 - a);
+}
+
+subtract(35);
+
+//3. Write a function that takes two numbers as an argument and multiplies them together, return and console log the result.
+function multiply(a, b) {
+  return console.log(a * b);
+}
+
+multiply(31, 3);

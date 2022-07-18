3a776f5669b258219017a6b576c04c6c5091f751
JonasKaruzas <jonas.karuzas@gmail.com>
2022-07-18 19:17:25 +0300

    visi aritmetiniai operatoriai

--- studentai/jonasKaruzas/lesson-4/operators/script.js
+++ studentai/jonasKaruzas/lesson-4/operators/script.js
@@ -10,6 +10,34 @@ function multiply(num1, num2) {
   return num1 * num2;
 }
 
-console.log(add(11, 22));
+function divideByTwo(num1) {
+  return num1 / 2;
+}
+
+function exponentByFive(num) {
+  return num ** 5;
+}
+
+function isEven(num1) {
+  return num1 % 2 === 0 ? true : false;
+}
+
+function addOne(num) {
+  num++;
+  return num;
+}
+
+function minusOne(num) {
+  num--;
+  return num;
+}
+
+console.log(add(4, 6));
 console.log(subtractFromHundred(98));
-console.log(multiply(2, 5));
+console.log(multiply(6, 5));
+console.log(divideByTwo(8));
+console.log(exponentByFive(2));
+console.log(isEven(8));
+console.log(isEven(9));
+console.log(addOne(8));
+console.log(minusOne(8));

8962eedc7b525fae01474f9f6d656be63ee85174
renaldas112 <renaldas112@gmail.com>
2022-07-18 18:42:34 +0300

    a

---
+++ studentai/renaldasBarzdaitis/lesson-4/arithmetic-operators/script.js
@@ -0,0 +1,17 @@
+function myFunction(num1, num2) {
+    return sum = num1 + num2;
+}
+
+console.log(myFunction(4, 12));
+
+function mySecondFunction(num3) {
+    return subst = 100 - num3;
+}
+
+console.log(mySecondFunction(15));
+
+function myThirdFunction(num4, num5) {
+    return sum = num4 * num5;
+}
+
+console.log(myThirdFunction(5, 5));

7ad8a26c6f13ce9c41ad8c969861d95e86e2fb04
kavaini <karolina.vainilkaite@gmail.com>
2022-07-18 19:02:01 +0300

    first attempt

---
+++ studentai/karolinaVainilkaite/Lesson-4/arithmetic operators/operator.js
@@ -0,0 +1,15 @@
+function sum(a, b) {
+    return(a + b);
+}
+console.log(1,3);
+
+function sub(c){
+    return c-100;
+
+}
+console.log (150);
+
+function multiply(d, e){
+    return d/e;
+}
+console.log(90, 3);

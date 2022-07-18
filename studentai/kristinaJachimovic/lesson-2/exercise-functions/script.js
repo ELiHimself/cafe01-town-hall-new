ea28ebe92be2ba097befd4c9daa19fe8091b286b
Kristina <kristtule@gmail.com>
2022-07-18 18:13:05 +0300

    homework

--- studentai/kristinaJachimovic/lesson-2/exercise-functions/script.js
+++ studentai/kristinaJachimovic/lesson-2/exercise-functions/script.js
@@ -17,6 +17,34 @@ console.log(greeting);
 
 function a() {
   console.log('lol');
-}
+};
 a();
+
+// let ivestis = prompt('Ivesk skaiciu: ');
+// function add() {
+//   let sum = 0;
+//   for (let i = 0; i < arguments.length; i++) {
+//     sum += arguments[i];
+//   }
+//   return sum - ivestis;
+// };
+// console.log(add(8, 7, 9));
+
+// let logo = prompt('Ivesk skaiciu: ');
+// function addNumber() {
+//   let sum = 0;
+//   for (let j = 0; j < arguments.length; j++) {
+//     sum += arguments[j];
+//   }
+//   return sum / logo;
+// }
+
+// console.log(addNumber(1, 1, 1));
+// console.log(addNumber(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+
+// arrow
+
+console.log(esam(`Dabar yra ${currentTime.getFullYear()} metai`));
+
+const vardas = (name) => `hello, ${name} !`;
 

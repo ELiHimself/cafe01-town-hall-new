d4d93f2b72737a068f3fe1d05d257bb6d0df1a88
Martynas Fetingis <105384042+mrtfe@users.noreply.github.com>
2022-07-20 19:13:28 +0300

    branch fix (#40)
    * branch fix
    
    * Fixed forlder names & changed var<->const
    
    * javascripting 10/20
    
    * javascripting exercise done
    
    * yesterday leftovers
    
    * 1 uzduotis
    
    * 2 uzduotis
    
    * 3 uzduotis
    
    * yesterday fixes after comments
    
    * 4 uzduotis + prettier 1,2,3 uzduotyje.
    
    * 4 uzduotis + prettier 1,2,3 uzduotyje
    
    * 5 nepadaryta - strigau
    
    * branch fix
    
    * Fixed forlder names & changed var<->const
    
    * javascripting 10/20
    
    * javascripting exercise done
    
    * yesterday leftovers
    
    * 1 uzduotis
    
    * 2 uzduotis
    
    * 3 uzduotis
    
    * yesterday fixes after comments
    
    * 4 uzduotis + prettier 1,2,3 uzduotyje.
    
    * 4 uzduotis + prettier 1,2,3 uzduotyje
    
    * 5 nepadaryta - strigau
    
    * 5 uzduotis
    
    * 6 uzduotis
    
    * 6 uzduotis fixed
    
    * 6 fixed
    
    * 7 uzduotis
    
    * 8 uzduotis
    
    * start of the lesson 3
    
    * pirma uzduotis
    
    * 2nd commit
    
    * test
    
    * changes based on comments
    
    Co-authored-by: Eli <eli.tamosauskas@redacreltd.com>

---
+++ studentai/martynasFetingis/lesson-3/aritmetic-operators/script.js
@@ -0,0 +1,64 @@
+// 1st function - SUM //
+
+function sum(a, b) {
+  return a + b;
+}
+console.log(sum(5, 7));
+
+// 2nd function - SUBTRACTION //
+
+function sub(a) {
+  return 100 - a;
+}
+console.log(sub(55));
+
+// 3rd function - MULTIPLICATION //
+
+function multi(a, b) {
+  return a * b;
+}
+console.log(multi(10, 5));
+
+// 4th function - Exponential //
+
+function expo(a, b) {
+  return a ** b;
+}
+console.log(expo(10, 3));
+
+// 5th function - Division //
+
+function div(a, b) {
+  return a / b;
+}
+console.log(div(10, 5));
+
+// 6th function - Modulus(remainder) //
+
+function mod(a, b) {
+  return a % b;
+}
+console.log(mod(10, 3));
+
+// 7th function - increment //
+
+function increment(a) {
+  return ++a;
+}
+console.log(increment(1));
+
+// 8th function - decrement //
+function decrement(a) {
+  return --a;
+}
+console.log(decrement(10));
+
+function myFunction(str) {
+  return str.slice(str.length - 3);
+}
+console.log(myFunction('helloWorld'));
+
+function sayHello() {
+  return 'Hello World!';
+}
+console.log(sayHello());

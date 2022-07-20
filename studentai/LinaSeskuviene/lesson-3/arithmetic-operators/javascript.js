ccce0d52023df7ca86570108342c505ca0baf73b
LinaSeskuviene <105384709+LinaSeskuviene@users.noreply.github.com>
2022-07-20 18:43:48 +0300

    Lina seskuviene (#46)
    * java first excercise
    
    * 7 exercises of 20
    
    * 7 excercises of 20
    
    * 8 excercises of 20
    
    * 10 excercises of 20
    
    * yesterday exercises+1today
    
    * 2excercise
    
    * Jonas Karuzas - lesson 2 - ex1 (#51)
    
    * test2
    
    * test2
    
    * Javascripting all 20 done
    
    * dar
    
    * panaudotas map
    
    * reduce test
    
    * lesson2 - ex1
    
    * 1-3 uzduotys
    
    * 4 uzduotis
    
    * 5 uzduotis
    
    * 6 uzduotis
    
    * 7 uzduotis
    
    * 8 uzduotis
    
    * 3 uzduotis su arrow function
    
    * minor fix'es
    
    * Update studentai/jonasKaruzas/lesson-2/exercise-functions/script.js
    
    Co-authored-by: Mindaugas Paskevicius <pask.mindaugas@gmail.com>
    
    * Jonas Karuzas - minor fix and IF to SWITCH (#58)
    
    * test2
    
    * test2
    
    * Javascripting all 20 done
    
    * dar
    
    * panaudotas map
    
    * reduce test
    
    * lesson2 - ex1
    
    * 1-3 uzduotys
    
    * 4 uzduotis
    
    * 5 uzduotis
    
    * 6 uzduotis
    
    * 7 uzduotis
    
    * 8 uzduotis
    
    * 3 uzduotis su arrow function
    
    * minor fix'es
    
    * Update studentai/jonasKaruzas/lesson-2/exercise-functions/script.js
    
    * 4 uzduotis - from if to switch
    
    * 6 uzduotis - prettier magic
    
    Co-authored-by: Mindaugas Paskevicius <pask.mindaugas@gmail.com>
    
    * 3excercise
    
    * 8 tasks finished (#53)
    
    * first three tasks
    
    * fourth exercise
    
    * fifth task done
    
    * task 6 done
    
    * task 7 done
    
    * last task 8 finished
    
    * quick fixes
    
    * quick fix
    
    * fixed what needed to be fixed
    
    * fixed
    
    * Edgaras andrejevas (#59)
    
    * 6 challenges done
    
    * 10 Javascripting challenges complete
    
    * Trying to make lesson 2 work
    
    * Javascripting atliktose užduotyse sudėjau skirybos ženklus
    
    * 4excercise
    
    * 5 excercise
    
    * 6 and 7 excercises
    
    * 6 and 7 excercises
    
    * exercise-data-types-and-and-variables
    
    * homework_javascripting_done
    
    * added some tasks
    
    * 7 exercises of 20
    
    * 7 excercises of 20
    
    * 8 excercises of 20
    
    * 10 excercises of 20
    
    * yesterday exercises+1today
    
    * 2excercise
    
    * 3excercise
    
    * 4excercise
    
    * 5 excercise
    
    * 6 and 7 excercises
    
    * 6 and 7 excercises
    
    * lesson 3 excercise 1
    
    * commit
    
    Co-authored-by: JonasKaruzas <60874740+JonasKaruzas@users.noreply.github.com>
    Co-authored-by: Mindaugas Paskevicius <pask.mindaugas@gmail.com>
    Co-authored-by: Titas Petrauskas <100712546+titasas@users.noreply.github.com>
    Co-authored-by: EdgarasAnd <105384015+EdgarasAnd@users.noreply.github.com>
    Co-authored-by: KristinaBal <105514019+KristinaBal@users.noreply.github.com>
    Co-authored-by: Eli <eli.tamosauskas@redacreltd.com>
    Co-authored-by: EliTamošauskas <tamosauskas09@gmail.com>

---
+++ studentai/LinaSeskuviene/lesson-3/arithmetic-operators/javascript.js
@@ -0,0 +1,30 @@
+function add(a, b) {
+    return a + b;
+}
+console.log(add(2, 5));
+
+function subtract(c) {
+    return 100 - c;
+}
+console.log(subtract(35));
+
+function multiply(d, e) {
+    return d * e;
+}
+console.log(multiply(6, 8));
+
+function exponentation(g) {
+    return g ** 3;
+}
+console.log(exponentation(5));
+
+function division(f, h) {
+    return f / h;
+}
+console.log(division(16, 4));
+
+function modulus(i) {
+    return 10 % i;
+}
+console.log(modulus(3));
+

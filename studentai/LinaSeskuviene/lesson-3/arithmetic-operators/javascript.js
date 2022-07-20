575ffb7c821a0e844175b9be995fba802b0e91a9
LinaSeskuviene <ses.lina@gmail.com>
2022-07-20 18:41:49 +0300

    commit

--- studentai/LinaSeskuviene/lesson-3/arithmetic-operators/javascript.js
+++ studentai/LinaSeskuviene/lesson-3/arithmetic-operators/javascript.js
@@ -12,3 +12,19 @@ function multiply(d, e) {
     return d * e;
 }
 console.log(multiply(6, 8));
+
+function exponentation(g) {
+    return g ** 3;
+}
+console.log(exponentation(5));
+
+function division(f, h) {
+    return f / h;
+}
+console.log(division(16, 4));
+
+function modulus(i) {
+    return 10 % i;
+}
+console.log(modulus(3));
+

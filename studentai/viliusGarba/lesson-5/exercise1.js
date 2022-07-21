f91a3c8a1fe12fdd1528e3a03611bc2c569e63f9
Vilius Garba <vilius.garba@gmail.com>
2022-07-21 20:26:16 +0300

    exercise 5

--- studentai/viliusGarba/lesson-5/exercise1.js
+++ studentai/viliusGarba/lesson-5/exercise1.js
@@ -1,12 +1,10 @@
-let money = 100.50
-let price = 105.00
-
-
+let money = 100.5;
+let price = 105.0;
 
 if (money > price) {
-    console.log("You paid extra, heres your change");
+  console.log('You paid extra, heres your change');
 } else if (money === price) {
-     console.log("You paid the exact amount, have a nice day");
+  console.log('You paid the exact amount, have a nice day');
 } else {
-     console.log("That's not enough, you still owe me money");
+  console.log("That's not enough, you still owe me money");
 }
--- studentai/viliusGarba/lesson-5/exercise2.js
+++ studentai/viliusGarba/lesson-5/exercise2.js
@@ -1,20 +1,19 @@
-let runner = "Kendyll";
+let runner = 'Kendyll';
 let position = 5;
-let medal = "";
+let medal = '';
 
-
-if (position === 1) {  
-    medal = "gold";
-} else if (position === 2) {   
-    medal = "silver";
-} else if (position === 3) {  
-    medal = "bronze";
-} else {  
-    medal = "pat on the back";
+if (position === 1) {
+  medal = 'gold';
+} else if (position === 2) {
+  medal = 'silver';
+} else if (position === 3) {
+  medal = 'bronze';
+} else {
+  medal = 'pat on the back';
 }
 
 if (position <= 3) {
-    console.log(runner + " received a " + medal + " medal.");
+  console.log(runner + ' received a ' + medal + ' medal.');
 } else {
-    console.log(runner + " received a " + medal);
+  console.log(runner + ' received a ' + medal);
 }

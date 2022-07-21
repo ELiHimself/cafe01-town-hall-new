ea7dee6229dca594c40786947a5ff3422387bad6
renaldas112 <renaldas112@gmail.com>
2022-07-21 18:31:06 +0300

    changed the files to different topics for my personal ****

--- studentai/renaldasBarzdaitis/personal lessons/for-loop.js
+++ studentai/renaldasBarzdaitis/personal lessons/for-loop.js
@@ -1,5 +1,38 @@
-const myArr = [2, 3, 4, 5, 6];
 
-for (let total = 0; total < myArr.lenght; i++){
-  console.log(myArr[total]);
+// while loops
+
+const ourArray = [];
+let i = 0;
+        //\/---------------------------                  //
+while (i < 5) {                     //|                  //In the code example above, the while loop will execute 5 times
+  ourArray.push(i);                 //|                     //and append the numbers 0 through 4 to ourArray.   //^
+  i++;                              //|                     //so console will print myArray[0, 1, 2, 3, 4]      //|
+}                                   //---------------------------------------------------|  ^  ^  ^  ^  ^  |    //|
+                                                                                       //|  1  2  3  4  5  |    //|
+                                                                                       //|5 times excecuted|-------
+const myArray = [];
+let i = 0;                                               //same while loop, but this time (i <=5) which means it will print 
+while (i <= 5) {                                         //[0, 1, 2, 3, 4, 5]
+  myArray.push(i);                                       //but the task requires to print the array in descending
+  i++;
 }
+console.log(myArray.reverse());                          //so .reverse comes in handy to flip the array.
+
+// Setup
+const myArray = [];
+
+for (let i = 1; i <= 9; i += 2){                         //pushes "odd" numbers to myArray
+  myArray.push(i);                                       //so myArray will have [1, 3, 5, 7, 9];
+}                                                        //because let i = 1; will start from 1 (instead of 0) and +=2 will add 2 every time
+                                                         //the cycle repeats until it gets to 9, because i <= 9;
+
+                                                         //add array elements to total, by adding each element to each other to get total of 20
+const myArr = [2, 3, 4, 5, 6];                           //because total = 0; so 0 + 2 + 3 + 4 + 5 + 6 = 20
+                                                         //                          |     myArr     |
+let total = 0;
+
+for (let i = 0; i < myArr.length; i++) {                 //let i = 0 (it starts from 0(index = 0), then i is smaller than myArr.length( length is how many
+  total += myArr[i];                                     //elements are in the array (2, 3, 4, 5, 6) = 5 elements), i++ adds a new cycle to i until it
+}                                                        //reaches the size of myArr.length, so when i = 5 it stops the cycle).
+                                                         //then "total" gets the total of all the elements in myArr added to each other.
+
---
+++ studentai/renaldasBarzdaitis/personal lessons/functions.js
@@ -0,0 +1,103 @@
+//functions with argument
+function functionWithArgs(a, b) {                       //funkcija kuri turi 2 arguments
+    console.log(a + b);                                 //konsole kuri 2 argumentus sudeda
+  }
+  
+  functionWithArgs(1, 2);                               //pakvieciau funkcija kuriois viduje yra argumentai kuriuos sudesime
+                                                        //gali buti ir zodziai a = "hello", b = "world" tada pakvietus
+                                                        //funkcija bus "hello world";
+
+//function with return statement
+function timesFive(num) {                               //funkcija kuri turi argumenta "num"
+  return num * 5;                                       //return skirtas tam kad grazintu atsakyma is uz funkcijos, siuo atveju
+}                                                       //funkcijos argumentas yra dauginamas is 5
+
+const answer = timesFive(5);                            //tai cia sukurus nauja kintamaji ir pakvietus funkcija, funkcijos 
+                                                        //argumente irasius 5, return ta funkcijos pakviesta argumenta "timesFive(5)"
+                                                        //padaugins is 5 kuris buvo funkcijos viduje
+//global scope variables
+//outside of function
+
+const myGlobal = 10;                                    //Variables which are defined outside of a function block have Global scope.
+                                                        //This means, they can be seen everywhere in your JavaScript code.
+
+function fun1() {
+  oopsGlobal = 5;                                       //Variables which are declared without the "let" or "const" keywords 
+                                                        //are automatically created in the global scope
+}
+
+function fun2() {                                       
+  var output = "";                                      
+  if (typeof myGlobal != "undefined") {                 //The inequality operator (!=) checks whether its two operands are not equal
+    output += "myGlobal: " + myGlobal;                  //this means that if "myGlobal" is "undefined" the result would be false, 
+  }                                                     //because they're equal
+  if (typeof oopsGlobal != "undefined") {               //but if "myGlobal" or "oopsGlobal" has any value than being empty (which would be"undefined")
+    output += " oopsGlobal: " + oopsGlobal;             //than the result would be true - which means they aren't equal
+  }                                                     //so this "if" checks whether "myGlobal" or "oopsGlobal" has any value, then it prints those
+                                                        //values into console
+  console.log(output);
+}
+
+//global and local variables
+
+function myLocalScope() {                               //si funkcija turi lokalu kintamaji "myVar" kuris prasideda zodziu "var", jeigu 
+  var myVar = "hello";                                  //kintamasis "myVar" neturetu "let", "var" arba "const" tada jis automatiskai taptu gloabliu.
+  console.log('inside myLocalScope', myVar);            //konsole isspausdina sakini ir "myVar" kintamaji
+}
+myLocalScope();                                         //pakviecia funkcija kuri turi lokalu kintamaji
+
+console.log('outside myLocalScope', myVar);             //konsole spausdina sakini ir lokalu kintamaji is funkcijos ribu ir gaunama klaida, kad "myVar"
+                                                        //kintamasis neegzistuoja "global scope"
+
+//local scope overwriting global                                               
+
+const outerWear = "T-Shirt";                            //global variable
+
+function myOutfit() {
+  const outerWear = "sweater";                          //local variable
+  
+  return outerWear;                                     //the local variable takes precedence over the global variable.
+}
+
+myOutfit();                                             //The function "myOutfit" will return the string "sweater" 
+                                                        //because the local version of the variable is present.
+
+//function without return statement
+
+let sum = 0;
+
+function addThree() {                                   //A function can include the return statement but it does not have to. 
+sum = sum + 3;                                          //In the case that the function doesn't have a return statement, when you call it, 
+}                                                       //the function processes the inner code but the returned value is undefined.
+
+function addFive() {                                    //addSum is a function without a return statement.
+  sum = sum + 5;                                        //The function will change the global sum variable but the returned value of the function is undefined.
+}
+
+addThree();
+addFive();
+
+//funkcijos return value tampa kintamojo value
+
+let processed = 0;  i                                   //kintamasis turi value 0
+
+function processArg(num) {                              //funkcija su argumentu "num"
+  return (num + 3) / 5;                                 //spredimas yra = funkcijos argumentas "num" prie jo pridedamas skaicius 3 ir suma padalinama is 5
+}                                                       
+
+processed = processArg(7);                              //pries tai sukurtas kintamasis su value "0" yra pakvieciamas ir jo naujas value tampa funkcijos
+                                                        //"processArg" sprendimo atsakymo value. [ (7 + 3) / 5] = 2
+                                                        //tai "let processed = 2"
+
+//nextInLine funkcija kuri atima skaiciu ir prideda i array
+
+function nextInLine(arr, item) {                        //funkcija kuri atims pirma masyvo skaiciu "1" is masyvo pradzios ir prides skaiciu "6" masyvo pabaigoje
+  arr.push(item);                                       //push prides skaiciu (item - 6)
+  return arr.shift();                                   //ir returns masyvo skaiciu su (shift - 1) shift atima skaiciu is masyvo pradzios o unshitft prideda i
+}                                                       //masyvo pradzia. o push prideda skaiciu i masyvo pabaiga
+
+const testArr = [1, 2, 3, 4, 5];                        //masyvas is kurio bus paimamas 1 is masyvo pradzios ir pridedamas 6 i masyvo pabaiga
+
+console.log("Before: " + JSON.stringify(testArr));      //konsole kuri isspausdins masyva pries pakeitimus
+console.log(nextInLine(testArr, 6));                    //pakvieciama funkcija i konsole su argumento value - (item = 6) 6 kuris yra pridedamas i masyvo pabaiga.
+console.log("After: " + JSON.stringify(testArr));       //konsole kuri isspausdins masyva po pakeitimu

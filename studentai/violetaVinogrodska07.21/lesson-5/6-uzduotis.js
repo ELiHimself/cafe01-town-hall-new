093749c007020cf32489a7de65428123284e5315
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-21 21:28:18 +0300

    2-budas veikia

--- studentai/violetaVinogrodska07.21/lesson-5/6-uzduotis.js
+++ studentai/violetaVinogrodska07.21/lesson-5/6-uzduotis.js
@@ -8,3 +8,14 @@ if(carMaker === 'Audi' || carMaker === 'BMW' || carMaker === 'Opel' || carMaker 
 } else if(carMaker === 'Audi' || carMaker === 'BMW' || carMaker === 'Opel' || carMaker === 'VW' && carYear < 2000 ) {
     console.log('Automobilis ' + carMaker + ' yra ' + ' 20 metu ir senesnis')
 };
+
+
+if(carMaker === 'Audi' || carMaker === 'BMW' || carMaker === 'Opel' || carMaker === 'VW') {
+    if(carYear >= 2010 &&  carYear <= 2020){
+        console.log('Automobilis ' + carMaker + ' yra ' + ' 10 metu')
+    } else if(carYear < 2010 && carYear >= 2000){
+        console.log('Automobilis ' + carMaker + ' yra ' + ' 20 metu')
+    } else if(carYear < 2000){
+        console.log('Automobilis ' + carMaker + ' yra ' + ' 20 metu ir senesnis')
+    }
+}

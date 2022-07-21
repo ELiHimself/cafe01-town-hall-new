313edaeb771ae8128fc4a36ef1060ff86761f5f4
renaldas112 <renaldas112@gmail.com>
2022-07-21 18:48:28 +0300

    Boolean 1 == "1" true, but 1 === "1" is false

---
+++ studentai/renaldasBarzdaitis/lesson-5/script.js
@@ -0,0 +1,20 @@
+Boolean()
+false
+Boolean(1)
+true
+Boolean("string")
+true
+Boolean("")
+false
+Boolean(function() {})
+true
+Boolean(Boolean)
+true
+Boolean([])
+true
+Boolean(undefined)
+false
+Boolean(1 == "1")
+true
+Boolean(1 === "1")
+false

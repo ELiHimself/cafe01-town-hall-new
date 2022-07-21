79ba79d08693939663c70abbe8c85e005b13af98
titasas <titas.petrauskas@gmail.com>
2022-07-21 20:58:41 +0300

    sixth task done

--- studentai/titasPetrauskas/lesson-5/exercises/second exercise - 6th task/script.js
+++ studentai/titasPetrauskas/lesson-5/exercises/second exercise - 6th task/script.js
@@ -1,15 +1,18 @@
 let carMaker = 'BMW';
-let carYear = 2020;
+let carYear = 2005;
 
-if (carYear > 1999 && carYear < 2023) {
-  console.log(`Your ${carMaker} car is ${2022 - carYear} years old.`)
-} else if (carYear < 2000 && carYear > 1885) {
-  console.log(`Your car is over 22 years old.`);
-} else if (carYear < 1886) {
-  console.log(`Your car came from the future.`);
+if (carMaker === 'BMW' || carMaker === 'Audi' || carMaker === 'Opel' || carMaker === 'Volkswagen') {
+  if (carYear > 1999 && carYear < 2023) {
+    console.log(`Your ${carMaker} car is ${2022 - carYear} years old.`)
+  } else if (carYear < 2000 && carYear > 1885) {
+    console.log(`Your ${carMaker} car is over 22 years old.`);
+  } else if (carYear < 1886) {
+    console.log(`Your ${carMaker} car came ${1886 - carYear} years from the future.`);
+  } else {
+    console.log(`Your ${carMaker} car hasn't been made yet.`);
+  }
 } else {
-  console.log(`Your car hasn't been made yet.`);
+  console.log (`${carMaker} is not in the database.`);
 }
 
-
-// Biski ne taip supratau uzduoti, taisysiu ka parasiau
+// Does the work. Ar per daug paprastai cia visgi?

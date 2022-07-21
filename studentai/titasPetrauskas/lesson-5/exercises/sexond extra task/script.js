b623f36de272a0c880c37ee47f6b0ddeae912b3c
titasas <titas.petrauskas@gmail.com>
2022-07-21 21:52:04 +0300

    tried to fix, still need help

--- studentai/titasPetrauskas/lesson-5/exercises/sexond extra task/script.js
+++ studentai/titasPetrauskas/lesson-5/exercises/sexond extra task/script.js
@@ -4,6 +4,7 @@ function leapYear(year) {
   } 
   return false;
 }
+// 1st try
 
 function daysInMonth(year, month) {
   if (month.toLowerCase() === 'january' || month.toLowerCase() === 'march' || month.toLowerCase() === 'may' || month.toLowerCase() === 'july' || month.toLowerCase() === 'august' || month.toLowerCase() === 'october' || month.toLowerCase() === 'december') {
@@ -17,5 +18,22 @@ function daysInMonth(year, month) {
     return `${month.charAt(0).toUpperCase() + month.slice(1)} has 28 days`;
   } else return `There is no such month as ${month}`;
 }
+
+
+// 2nd try
+// function daysInMonth(year, month) {
+//   if (month === 'january' || month === 'march' || month === 'may' || month === 'july' || month === 'august' || month === 'october' || month === 'december') {
+//     return `${month} has 31 day`;
+//   } else if (month === 'april' || month === 'june' || month === 'september' || month === 'november') {
+//     return `${month} has 30 days`;
+//   } else if (month === 'february') {
+//     if (leapYear === true) {
+//       return `${month} has 29 days`;
+//     }
+//     return `${month} has 28 days`;
+//   } else return `There is no such month as ${month}`;
+// }
+
+
 
 daysInMonth(1236, 'February');

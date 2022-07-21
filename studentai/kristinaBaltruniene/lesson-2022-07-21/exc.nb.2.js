1912a75b791ce79dad396e71b3cf6386e79d89df
KristinaBal <105514019+KristinaBal@users.noreply.github.com>
2022-07-21 20:30:10 +0300

    classwork

---
+++ studentai/kristinaBaltruniene/lesson-2022-07-21/exc.nb.2.js
@@ -0,0 +1,15 @@
+let runner = "Kendyll";
+let position = 2;
+let medal;
+
+if (position === 1) {
+    medal = "gold";
+} else if (position === 2) {
+    medal = "silver";
+} else if (position === 3) {
+    medal = "bronze";
+} else {
+    medal = "pat on the back";
+}
+console.log(runner + "received a" + medal + "medal.");
+/*Kendyllreceived asilvermedal.*/

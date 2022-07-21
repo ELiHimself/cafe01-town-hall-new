80b501c9393a104124130a91d1a116baaf6e408f
titasas <titas.petrauskas@gmail.com>
2022-07-21 21:05:18 +0300

    quick update for the sixth task

--- studentai/titasPetrauskas/lesson-5/exercises/second exercise - 6th task/script.js
+++ studentai/titasPetrauskas/lesson-5/exercises/second exercise - 6th task/script.js
@@ -1,18 +1,22 @@
-let carMaker = 'BMW';
+let carMaker = 'Seat';
 let carYear = 2005;
 
-if (carMaker === 'BMW' || carMaker === 'Audi' || carMaker === 'Opel' || carMaker === 'Volkswagen') {
+function carYearCounter(carYear, carMaker) {
   if (carYear > 1999 && carYear < 2023) {
-    console.log(`Your ${carMaker} car is ${2022 - carYear} years old.`)
+   console.log(`Your ${carMaker} car is ${2022 - carYear} years old.`)
   } else if (carYear < 2000 && carYear > 1885) {
-    console.log(`Your ${carMaker} car is over 22 years old.`);
+   console.log(`Your ${carMaker} car is over 22 years old.`);
   } else if (carYear < 1886) {
-    console.log(`Your ${carMaker} car came ${1886 - carYear} years from the future.`);
+   console.log(`Your ${carMaker} car came ${1886 - carYear} years from the future.`);
   } else {
-    console.log(`Your ${carMaker} car hasn't been made yet.`);
+   console.log(`Your ${carMaker} car hasn't been made yet.`);
   }
+}
+
+if (carMaker === 'BMW' || carMaker === 'Audi' || carMaker === 'Opel' || carMaker === 'Volkswagen') {
+  carYearCounter(carYear, carMaker);
 } else {
-  console.log (`${carMaker} is not in the database.`);
+  console.log (`The car brand \"${carMaker}\" is not in the database.`);
 }
 
 // Does the work. Ar per daug paprastai cia visgi?

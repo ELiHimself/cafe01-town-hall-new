7611aa15f1bc15c01b597fc07a2d4f31d922676f
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-07-21 20:07:34 +0300

    exercise-4

---
+++ studentai/martynasFetingis/lesson-5/exercise-4/script.js
@@ -0,0 +1,17 @@
+function musicGroup(ppl) {
+  if (ppl <= 0) {
+    return 'ne grupė';
+  } else if (ppl === 1) {
+    return 'solo';
+  } else if (ppl === 2) {
+    return 'duetas';
+  } else if (ppl === 3) {
+    return 'trio';
+  } else if (ppl === 4) {
+    return 'kvartetas';
+  } else if (ppl > 4) {
+    return 'didelė grupė';
+  }
+}
+
+console.log(musicGroup(3));

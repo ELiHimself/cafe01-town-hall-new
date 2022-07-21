42ffb63bd41466c937ed21117d2d4858ab56ef1a
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-07-21 21:21:03 +0300

    Exercise-6 fixed

--- studentai/martynasFetingis/lesson-5/exercise-6/script.js
+++ studentai/martynasFetingis/lesson-5/exercise-6/script.js
@@ -2,15 +2,15 @@ let carMaker = ['Audi', 'BMW', 'Opel', 'VW'];
 let carYear = 2000;
 
 function carVertical(carMaker, carYear) {
-  if (carMaker.includes('Audi', 'BMW', 'Opel', 'VW') || carYear >= 2010 || carYear <= 2020) {
+  if (carMaker.includes('Audi', 'BMW', 'Opel', 'VW') && carYear >= 2010 && carYear <= 2020) {
     return 'Automobilis ' + carMaker + ' yra 10 metų.';
-  } else if (carMaker.includes('Audi', 'BMW', 'Opel', 'VW') || carYear >= 2000 || carYear <= 2010) {
+  } else if (carMaker.includes('Audi', 'BMW', 'Opel', 'VW') && carYear >= 2000 && carYear <= 2010) {
     return 'Automobilis ' + carMaker + ' yra 20 metų.';
-  } else if (carMaker.includes('Audi', 'BMW', 'Opel', 'VW') || carYear < 2000) {
+  } else if (carMaker.includes('Audi', 'BMW', 'Opel', 'VW') && carYear < 2000) {
     return 'Automobilis ' + carMaker + ' yra 20 metų ir senesnis.';
   } else if (!carMaker.includes('Audi', 'BMW', 'Opel', 'VW')) {
     return 'Automobilis ' + carMaker + ' nėra įtrauktas į duomenų bazę.';
   }
 }
 
-console.log(carVertical('Renault', 2005));
+console.log(carVertical('Renault', 1980));

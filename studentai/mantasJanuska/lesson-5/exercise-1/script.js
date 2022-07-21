5c9edb851bc006c8dc37609f6c25f8b35242a716
Panucio31 <m.januska1990@gmail.com>
2022-07-21 19:35:03 +0300

    exercise 3 started

---
+++ studentai/mantasJanuska/lesson-5/exercise-1/script.js
@@ -0,0 +1,10 @@
+let money = 100.50;
+let price = 105.50;
+
+if (money > price) {
+    console.log("you paid extra, here is your change")
+} else if (money === price) {
+    console.log('you paid the exact amount, have nice day')
+} else {
+    console.log('that is not enough, you still owe me money')
+}

72641c8e1701b687e102c656a2b88db0405064f9
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-21 19:29:51 +0300

    1 uzduotis

---
+++ studentai/violetaVinogrodska07.21/lesson-5/1-uzduotis.js
@@ -0,0 +1,10 @@
+let money = 100.50;
+let price = 105.50;
+
+if (money > price) {
+    console.log('You paid extra, here is change');
+} else if (money === price) {
+    console.log('You paid exact amount');
+} else {
+    console.log('Thats not enough, you still owe me money');
+}

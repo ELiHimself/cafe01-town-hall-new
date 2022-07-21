287c81e8bd4261c7fadc66e7abfd95d20b5ce75a
Panucio31 <m.januska1990@gmail.com>
2022-07-21 20:37:25 +0300

    exercise 6 finished

---
+++ studentai/mantasJanuska/lesson-5/exercise-5/script.js
@@ -0,0 +1,21 @@
+const golfNames = ["hole-in-one", "eagle", "birdie", "Par", "bogey", "double bogey", "go Home!"];
+
+function score(par, hits) {
+    if (hits === 1){
+        return golfNames[0];
+    }else if (hits <= par - 2) {
+        return golfNames[1];
+    }else if (hits <= par - 1) {
+        return golfNames[2];
+    }else if (hits <= par) {
+        return golfNames[3];
+    }else if (hits <= par + 1) {
+        return golfNames[4];
+    }else if (hits <= par + 2) {
+        return golfNames[5];
+    }else {
+        return golfNames[6];
+    }
+
+}
+console.log(score(5, 4));

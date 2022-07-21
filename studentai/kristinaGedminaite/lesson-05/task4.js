db5ccd5e9feaa41407d614a1f46aed557a5b3c1b
Kristina Gedminaite <105384146+nechbet@users.noreply.github.com>
2022-07-21 20:29:45 +0300

    fixed spacing

--- studentai/kristinaGedminaite/lesson-05/task4.js
+++ studentai/kristinaGedminaite/lesson-05/task4.js
@@ -1,13 +1,13 @@
-if (muzikantas<=0) {
+if (muzikantas <= 0) {
     console.log("ne grupe");
-}else if (muzikantas===1) {
+} else if (muzikantas === 1) {
     console.log("solo");
-}else if (muzikantas===2) {
+} else if (muzikantas === 2) {
     console.log("duetas");
-}else if (muzikantas===3) {
+} else if (muzikantas === 3) {
     console.log("trio");
-}else if (muzikantas===4) {
+} else if (muzikantas === 4) {
     console.log("kvartetas");
-}else {
+} else {
     console.log("didele grupe");
 }

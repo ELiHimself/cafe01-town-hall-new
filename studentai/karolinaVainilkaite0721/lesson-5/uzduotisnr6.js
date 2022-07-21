afab4f8c9eda0541fec5a863e3f8aa00bad8ace2
kavaini <karolina.vainilkaite@gmail.com>
2022-07-21 21:05:14 +0300

    uzduotisnr6

---
+++ studentai/karolinaVainilkaite0721/lesson-5/uzduotisnr6.js
@@ -0,0 +1,15 @@
+let carMaker = 'Audi';
+let carYear = 2000;
+
+if(carMaker === "Audi" ||carMaker === "BMW" || carMaker === "VW" || carMaker === "Opel" ){
+    if(carYear > 2020){
+        console.log("Automobilis " + carMaker + " yra naujas");
+    }else if(carYear <= 2010 || carYear <= 2020){
+        console.log("Automobilis " + carMaker + " yra 10 metų");
+    }else if(carYear <= 2000 || carYear <= 2010){
+        console.log("Automobilis " + carMaker + " yra 20 metų");
+    }else{
+        console.log("Automobilis " + carMaker + " yra 20 metų ir senesnis");
+    }
+}
+

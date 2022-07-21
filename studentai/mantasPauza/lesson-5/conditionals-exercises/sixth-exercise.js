d65897f8f5d1295355cf2be0af3074d308e5917a
Mantas <mantaspauza@gmail.com>
2022-07-21 21:28:47 +0300

    fixed sixth exercise

--- studentai/mantasPauza/lesson-5/conditionals-exercises/sixth-exercise.js
+++ studentai/mantasPauza/lesson-5/conditionals-exercises/sixth-exercise.js
@@ -1,13 +1,11 @@
-let carYearEnding = ' ';
-
 function getCarYear(carYear) {
     const yearRange = new Date().getFullYear() - carYear;
 
 
     if ( inRange(yearRange, 1, 9) || inRange(yearRange, 21, 29) || inRange(yearRange, 31, 39) || inRange(yearRange, 41, 49) || inRange(yearRange, 51, 59) || inRange(yearRange, 61, 69) || inRange(yearRange, 71, 79) || inRange(yearRange, 81, 89) || inRange(yearRange, 91, 99) ) {  
-        return carYearEnding = ' metai';
+        return ' metai';
     } else if (inRange(yearRange, 10, 20) || inRange(yearRange, 30, 40) || inRange(yearRange, 50, 60) || inRange(yearRange, 70, 80) || inRange(yearRange, 90, 100)) {
-        return carYearEnding = ' metų';
+        return ' metų';
     }
 }
 
@@ -17,17 +15,12 @@ function inRange(number, min, max) {
 
 function getCarInfo(carMaker, carYear) {
     
-    if (carMaker === 'Audi' || 'BMW' || 'Opel' || 'VW') {
+    if (['Audi', 'BMW', 'Opel', 'VW'].includes(carMaker)) {
         return 'Automobiliui ' + carMaker +  ' yra ' + (new Date().getFullYear() - carYear) + getCarYear(carYear) + '.';
+    } else {
+        return 'doesnt exist';
     }
-
 }
-// metai = 1-9; 21-29; 31-39; ..... 99; 101-109;
-
-//metų = 10-20; 30; 40; ..... 100; 110-120;
-
-
-
 
 
 console.log(getCarInfo('Audi', 2020));
@@ -37,3 +30,5 @@ console.log(getCarInfo('Opel', 2011));
 console.log(getCarInfo('BMW', 2000));
 console.log(getCarInfo('Opel', 1969));
 console.log(getCarInfo('Audi', 1922));
+console.log(getCarInfo('BMW', 1969));
+console.log(getCarInfo('Renault', 2020));

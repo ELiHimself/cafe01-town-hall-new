d738317fd73c861c0143e6ec88923102f2ff7d92
renaldas112 <renaldas112@gmail.com>
2022-07-21 19:47:23 +0300

    done

---
+++ studentai/renaldasBarzdaitis/lesson-5/uzduotis-2.js
@@ -0,0 +1,18 @@
+let runner = "Kendyll";
+let position = 4;
+let medal;
+
+if (position === 1) {
+    medal = "gold";
+    console.log(runner + " received a " + medal + " medal");
+} else if (position === 2) {
+    medal = "silver";
+    console.log(runner + " received a " + medal + " medal");
+} else if (position === 3) {
+    medal = "bronze";
+    console.log(runner + " received a " + medal + " medal");
+} else {
+    medal = "pat on the back";
+    console.log(runner + " received a " + medal);
+}
+

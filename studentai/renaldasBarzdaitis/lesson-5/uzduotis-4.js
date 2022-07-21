e483cb7916dff20428720c80d1a7914792ab1255
renaldas112 <renaldas112@gmail.com>
2022-07-21 20:02:04 +0300

    doneee

---
+++ studentai/renaldasBarzdaitis/lesson-5/uzduotis-4.js
@@ -0,0 +1,15 @@
+let band = 5;
+
+if (band <= 0) {
+    console.log("ne grupe");
+} else if (band === 1) {
+    console.log("solo");
+} else if (band === 2) {
+    console.log("duetas");
+} else if (band === 3) {
+    console.log("trio");
+} else if (band === 4) {
+    console.log("kvartetas");
+} else {
+    console.log("didele grupe");
+}

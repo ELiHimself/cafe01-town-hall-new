ed8cd891aa2fc85dd62c08ee3443542b49df1473
Mantas <mantaspauza@gmail.com>
2022-07-21 19:37:51 +0300

    third exercise done

---
+++ studentai/mantasPauza/lesson-5/conditionals-exercises/third-exercise.js
@@ -0,0 +1,23 @@
+let number = 25;
+
+if (number % 2 === 0) {
+    console.log("Skaičius yra lyginis");
+} else {
+    console.log("Skaičius yra nelyginis");
+} 
+
+number = 1;
+
+if (number % 2 === 0) {
+    console.log("Skaičius yra lyginis");
+} else {
+    console.log("Skaičius yra nelyginis");
+} 
+
+number = 10;
+
+if (number % 2 === 0) {
+    console.log("Skaičius yra lyginis");
+} else {
+    console.log("Skaičius yra nelyginis");
+} 

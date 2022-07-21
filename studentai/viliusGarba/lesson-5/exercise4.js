44862f3d36aa93bc1c6e01babb87af7f44334be8
Vilius Garba <vilius.garba@gmail.com>
2022-07-21 20:11:44 +0300

    commit

---
+++ studentai/viliusGarba/lesson-5/exercise4.js
@@ -0,0 +1,15 @@
+let numberOfMusicians = 5;
+
+if (numberOfMusicians <= 0) {
+    console.log("ne grupė")
+} else if (numberOfMusicians === 1) {
+    console.log("solo")
+} else if (numberOfMusicians === 2) {
+    console.log("duetas")
+} else if (numberOfMusicians === 3) {
+    console.log("trio") 
+} else if (numberOfMusicians === 4) {
+    console.log("kvartetas")
+} else {
+    console.log("didelė grupė")
+}

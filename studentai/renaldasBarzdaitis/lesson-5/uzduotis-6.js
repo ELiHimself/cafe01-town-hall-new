aca4a8790c8dba8d24cc8ae357bde03a6c2729f5
renaldas112 <renaldas112@gmail.com>
2022-07-21 21:00:01 +0300

    perdareu, dabar bsk logiskiau

--- studentai/renaldasBarzdaitis/lesson-5/uzduotis-6.js
+++ studentai/renaldasBarzdaitis/lesson-5/uzduotis-6.js
@@ -1,13 +1,17 @@
 let carMaker = "Audi";
-let carYear = "2015";
+let carYear = "2023";
 
 if (carMaker === "BMW" || carMaker === "Audi" || carMaker === "VW" || carMaker === "Opel") {
-    if (carYear >= "2010" && carYear <= "2020") {
-        console.log("Automobilis " + carMaker + " yra " + carYear + " metu");
-    } else if (carYear >= "2000" && carYear <= "2010") {
-        console.log("Automobilis " + carMaker + " yra " + carYear + " metu");
+    if (carYear === "2022") {
+        console.log("Automobilis " + carMaker + " yra " + " siu metu");
+    } else if (carYear > "2022") {
+        console.log("Automobilis " + carMaker + " negali buti " + carYear);
+    } else if (carYear === "2022") {
+        console.log("Automobilis " + carMaker + " yra " + " siu metu");
+    } else if (carYear >= "2010" && carYear <= "2022" || carYear >= "2000" && carYear <= "2010") {
+        console.log("Automobilis " + carMaker + " yra " + (2022 - carYear) + " metu");
     } else if (carYear < "2000") {
-        console.log("Automobilis " + carMaker + " yra " + carYear + " metu ir senesnis");
+        console.log("Automobilis " + carMaker + " yra " + " 20 metu ir senesnis");
     }
 } else {
     console.log(carMaker + " automobilis sarase neegzistuoja");

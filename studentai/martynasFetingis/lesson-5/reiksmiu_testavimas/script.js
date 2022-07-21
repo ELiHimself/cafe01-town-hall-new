f621775b8b03032e35b88f35dfc83a7b81f6123b
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-07-21 18:37:10 +0300

    papildymas

--- studentai/martynasFetingis/lesson-5/reiksmiu_testavimas/script.js
+++ studentai/martynasFetingis/lesson-5/reiksmiu_testavimas/script.js
@@ -2,5 +2,11 @@ const tekstas = ''; // false
 const tekstas2 = 'txt'; // "true"
 const num1 = 2; // tru
 const num2 = 0; // false
-const array = []; // true
+const array = ['']; // true
 const array2 = [1, 2, 3, 4, 5]; // true
+const undf = undefined; // false
+const nl = null; // false
+const check = function () {}; // true
+const more = 1 > 2; // false
+const less = 1 < 2; // false
+const equal = 1 == 1; // true

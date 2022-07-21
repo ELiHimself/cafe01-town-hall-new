4e6020f6fc2699bb183acc626bdb63aec4acd0fd
gieprie <giedreprieskienyte01@gmail.com>
2022-07-21 20:29:03 +0300

    4 done

---
+++ studentai/giedrePrieskienyte/lesson - 5/js-reiksmiu-testavimas/task4.js
@@ -0,0 +1,16 @@
+let group = 2;
+
+if (group >= 0){
+    console.log("ne grupe");
+} else if(group === 1){
+    console.log("solo");
+} else if (group === 2){
+    console.log("duetas");
+} else if (group === 3){
+    console.log("trio");
+} else if (group === 4){
+    console.log("kvartetas");
+} else if (group <= 4){
+    console.log("didele grupe");
+}
+

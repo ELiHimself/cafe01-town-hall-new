0c798808aadb85472679e9dfd12500646e4db87d
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-21 20:01:37 +0300

    4-uzd

---
+++ studentai/violetaVinogrodska07.21/lesson-5/4-uzduotis.js
@@ -0,0 +1,15 @@
+let grupeNariuSkaicius = 25;
+
+if(grupeNariuSkaicius == 0) {
+    console.log("ne grupe");
+} else if(grupeNariuSkaicius == 1) {
+    console.log("solo");
+} else if(grupeNariuSkaicius == 2) {
+    console.log("duetas");
+} else if(grupeNariuSkaicius == 3) {
+    console.log("trio");
+} else if(grupeNariuSkaicius == 4) {
+    console.log("kvartetas");
+} else { 
+    console.log("didele grupe");
+};

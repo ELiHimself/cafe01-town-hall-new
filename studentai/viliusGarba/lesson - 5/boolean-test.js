77021fd4c6be25f7080f61e7960df8d6a852080b
Vilius Garba <vilius.garba@gmail.com>
2022-07-21 18:29:05 +0300

    commit

---
+++ studentai/viliusGarba/lesson - 5/boolean-test.js
@@ -0,0 +1,13 @@
+Boolean() //false
+Boolean(1) //true
+Boolean(0) //false
+Boolean(-1) //true
+Boolean("") //false
+Boolean("Hello World") //true
+Boolean(["", 1, {}]) //true
+Boolean({}) //true
+Boolean(undefined) //false
+Boolean(null) //false
+Boolean(1 === 1) // true
+Boolean(1 === 5) //false
+Boolean(5 > 6 || 4 > 5) //false

2f9eb1618d94d84a6f803751d44c9208fc1579d0
Vilius Garba <vilius.garba@gmail.com>
2022-07-25 17:12:47 +0300

    commit

--- studentai/viliusGarba/lesson-5/exercise7.js
+++ studentai/viliusGarba/lesson-5/exercise7.js
@@ -1,7 +1,7 @@
 let year = 2024;
 
 function isLeapYear(year) {
-  if (year % 4 === 0) {
+  if (year % 4 === 0 || year % 100 === 1) {
     console.log('The year ' + year + ' is a leap year');
   } else {
     console.log('The year ' + year + ' is not a leap year');

262b78717d0939e92343b4ab4c88c744d21361fa
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-27 19:15:45 +0300

    2 uzduotis

--- studentai/violetaVinogrodska/liepos-27/exercise-1/script.js
+++ studentai/violetaVinogrodska/liepos-27/exercise-1/script.js
@@ -1,7 +1,18 @@
+
+//1 uzduotis
 for ( let i = 2; i <= 10;  i++) {
     if(i % 2 === 0){
-        console.log(i)
-    } else {
-        console.log('Nera')
+        console.log(i);
     }
-}
+};
+
+//2 uzduotis
+
+function getLaugh(amount) {
+    let laugh = '';
+    for (let i = 0; i < amount; i++) {
+        laugh += 'ha';
+    }
+    return laugh + '!';
+};
+console.log(getLaugh(4));

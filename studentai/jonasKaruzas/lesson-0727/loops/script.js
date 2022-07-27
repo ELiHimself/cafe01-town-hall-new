dadfc6c10f011dc5bfbb412fd597037a6d681567
JonasKaruzas <jonas.karuzas@gmail.com>
2022-07-27 18:34:09 +0300

    Uzduotis 2

--- studentai/jonasKaruzas/lesson-0727/loops/script.js
+++ studentai/jonasKaruzas/lesson-0727/loops/script.js
@@ -3,3 +3,15 @@
 for (let i = 2; i <= 10; i++) {
   i % 2 === 0 ? console.log(i) : undefined;
 }
+
+// uzduotis 2 ---------------
+
+function getLaugh(amount) {
+  result = '';
+  for (let i = 0; i < amount; i++) {
+    result += 'ha';
+  }
+  return result;
+}
+
+console.log(getLaugh(5));

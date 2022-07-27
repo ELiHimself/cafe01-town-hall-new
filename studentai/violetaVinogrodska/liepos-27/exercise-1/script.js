7e7f430d545315c8766746f10fe0ec1f9e5cdbc1
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-27 22:02:47 +0300

    commit

--- studentai/violetaVinogrodska/liepos-27/exercise-1/script.js
+++ studentai/violetaVinogrodska/liepos-27/exercise-1/script.js
@@ -36,3 +36,39 @@ console.log(getHighestNumber(34, 7, 8, 55, 6, 7, 9, 52, 56));
 for (let x = 9; x >= 1; x--){
     console.log('hello ' + x);
 }
+
+//5 uzduotis
+function createTree(height) {
+    for (let i = 1; i <= height; i++) {
+        printRow(i, height);
+    }
+}
+
+function printRow(rowNumber, height) {
+    const rowWidth = height * 2 - 1;
+    const starCount = rowNumber * 2 - 1;
+    const space = (rowWidth - starCount) / 2;
+
+    console.log(' '.repeat(space) + '*'.repeat(starCount) + ' '.repeat(space));
+}
+
+createTree(5);
+
+//5+ uzduotis
+//   *
+//  * *
+// * * * ...
+
+function makeNewTree(aukstis){
+    for (let i = 1; i <= aukstis; i++) {
+        createRow(i, aukstis);
+    }
+}
+function createRow(numberRow, aukstis) {
+    const plotis = aukstis * 2;
+    const zvaigzdute = numberRow * 2;
+    const tarpelis = (plotis - zvaigzdute) / 2;
+
+    console.log(' '.repeat(tarpelis) + '*'.repeat(zvaigzdute) + ' '.repeat(tarpelis));
+}
+makeNewTree(5);

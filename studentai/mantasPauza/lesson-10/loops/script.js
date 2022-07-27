7c7c605d0553c8373135376a36daec49ebb2c72d
Mantas <mantaspauza@gmail.com>
2022-07-27 18:36:43 +0300

    second done

--- studentai/mantasPauza/lesson-10/loops/script.js
+++ studentai/mantasPauza/lesson-10/loops/script.js
@@ -6,3 +6,16 @@ for (let i = 1; i <= 10; i++) {
         console.log(i);
     }
 }
+
+
+// ***************************************** Second exercise 
+
+function getLaugh(amount) {
+    let laugh = "";
+    for (let i = 0; i < amount; i++) {
+        laugh += "ha";
+    }
+    return laugh + "!";
+}
+
+console.log(getLaugh(5));

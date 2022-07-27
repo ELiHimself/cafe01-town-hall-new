67afc5a3bfd7ea838ab1cf96d9ea296e9a674c2f
heeyniknik <nikole.official@outlook.com>
2022-07-27 19:04:53 +0300

    1uzd

--- studentai/nikoleGrablevska/lesson-5 07.27/1uzd.js
+++ studentai/nikoleGrablevska/lesson-5 07.27/1uzd.js
@@ -1,11 +1,5 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-let evenNumbers = []
-
-for (let i = 0; i < numbers.length; i++) {
-    if (numbers[i] % 2 === 0);
-    evenNumbers.push(numbers[i]);
+for (let i = 1; i <=10; i++) {
+    if (i % 2 === 0) {
+    console.log(i);
+    }
 }
-
-console.log(evenNumbers);
-
-//nedaeina:'))))

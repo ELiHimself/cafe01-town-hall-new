a1e540f7401b911bb63fe39b8bd955050222b5b4
titasas <titas.petrauskas@gmail.com>
2022-07-27 19:27:07 +0300

    changed exercise 3 without MathMax

--- studentai/titasPetrauskas/lesson-6/3rd exercise/script.js
+++ studentai/titasPetrauskas/lesson-6/3rd exercise/script.js
@@ -1,5 +1,33 @@
+// function getHighestNumber(...args) {
+//   return Math.max(...args);
+// }
+
+// console.log(getHighestNumber(12, 6, 654, 44));
+
+// ***************************************************
+
+// function getHighestNumber(...args) {
+//   let highNum = 0;
+//   for (let arg of args) {
+//     if (args[arg] > args[arg + 1]) {
+//       highNum = args[arg];
+//     } else {
+//       highNum = args[arg + 1];
+//     }
+//     return console.log(highNum);
+//   }
+// }
+// getHighestNumber(12, 6, 654, 44);
+
 function getHighestNumber(...args) {
-  return Math.max(...args);
+  console.log(args);
+  let highNum = 0;
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > highNum) {
+      highNum = args[i];
+    }    
+  } 
+  return `The highest number is ${highNum}`; 
 }
 
-console.log(getHighestNumber(12, 6, 654, 44));
+console.log(getHighestNumber(12, 123, 4256, 12, 777, 1));

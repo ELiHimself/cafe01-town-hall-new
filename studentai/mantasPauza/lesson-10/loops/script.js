f76728b04da49f076ee8b4628bcb25d1567cc059
Mantas <mantaspauza@gmail.com>
2022-07-27 20:26:27 +0300

    used prettier

--- studentai/mantasPauza/lesson-10/loops/script.js
+++ studentai/mantasPauza/lesson-10/loops/script.js
@@ -1,21 +1,19 @@
-
 // ***************************************** First exercise
 
 for (let i = 1; i <= 10; i++) {
-    if (i % 2 === 0) {
-        console.log(i);
-    }
+  if (i % 2 === 0) {
+    console.log(i);
+  }
 }
 
-
-// ***************************************** Second exercise 
+// ***************************************** Second exercise
 
 function getLaugh(amount) {
-    let laugh = "";
-    for (let i = 0; i < amount; i++) {
-        laugh += "ha";
-    }
-    return laugh + "!";
+  let laugh = '';
+  for (let i = 0; i < amount; i++) {
+    laugh += 'ha';
+  }
+  return laugh + '!';
 }
 
 console.log(getLaugh(5));
@@ -23,39 +21,38 @@ console.log(getLaugh(5));
 // ***************************************** Third exercise
 
 const getHighestNumber = (...args) => {
-    let highest = 0;
-    for (let i = 0; i < args.length; i++) {
-        if (highest < args[i]) {
-            highest = args[i];
-        }
+  let highest = 0;
+  for (let i = 0; i < args.length; i++) {
+    if (highest < args[i]) {
+      highest = args[i];
     }
-    return highest;
-}
+  }
+  return highest;
+};
 
 console.log(getHighestNumber(1, 20, 3));
 
 // ***************************************** Fourth exercise
 
-
 for (let i = 9; i >= 1; i--) {
-    console.log('hello ' + i);
+  console.log('hello ' + i);
 }
 
-// ***************************************** Fifth exercise 
+// ***************************************** Fifth exercise
 
 function createTree(height) {
-    for ( var i = 0; i < height ; i++ ) {
-        let branch = '+';
-        let space = ' ';
+  for (let i = 0; i < height; i++) {
+    let branch = '+';
+    let space = ' ';
 
-        for ( let j = 1; j <= i; j++ ) {
-            branch = branch + '++';            
-        }
-           
-        let spacesBefore = space.repeat(height-i-1);
-        branch = spacesBefore + branch;
-        console.log(branch);
+    for (let j = 1; j <= i; j++) {
+      branch = branch + '++';
     }
+
+    let spacesBefore = space.repeat(height - i - 1);
+    branch = spacesBefore + branch;
+    console.log(branch);
+  }
 }
 
 createTree(5);

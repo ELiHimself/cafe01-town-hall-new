8e1f476efae39d38cf29951ef5c1d2d57068f06e
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-27 20:01:55 +0300

    3-uzd

--- studentai/violetaVinogrodska/liepos-27/exercise-1/script.js
+++ studentai/violetaVinogrodska/liepos-27/exercise-1/script.js
@@ -4,7 +4,7 @@ for ( let i = 2; i <= 10;  i++) {
     if(i % 2 === 0){
         console.log(i);
     }
-};
+}
 
 //2 uzduotis
 
@@ -14,5 +14,19 @@ function getLaugh(amount) {
         laugh += 'ha';
     }
     return laugh + '!';
-};
+}
 console.log(getLaugh(4));
+
+//3 uzduotis
+
+function getHighestNumber(...args) {
+    console.log(args);
+    let max = 0;
+    for (let i = 0; i < args.length; i++) {
+        if (args[i] > max){
+            max = args[i];
+        }      
+    }
+    return max;
+}
+console.log(getHighestNumber(34, 7, 8, 55, 6, 7, 9, 52, 56));

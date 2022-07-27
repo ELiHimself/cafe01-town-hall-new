1a7766700c9741ef5a0e8674354d7d7d2c11d908
JonasKaruzas <jonas.karuzas@gmail.com>
2022-07-27 20:01:32 +0300

    uzduotis 5 - dabar consol logina daug kartu.

--- studentai/jonasKaruzas/lesson-0727/loops/script.js
+++ studentai/jonasKaruzas/lesson-0727/loops/script.js
@@ -33,3 +33,55 @@ console.log(getHighestNumber(34, 7, 8, 55, 6, 7, 9, 52, 56));
 for (let x = 9; x >= 1; x--) {
   console.log('hello ' + x);
 }
+// uzduotis 5 ---------------
+
+// function createTree(height) {
+//   let rowLength = height * 2 + 1;
+//   let row = [];
+//   let starCount = 1;
+
+//   for (let i = 0; i < height; i++) {
+//     let spaceLength = rowLength - starCount;
+//     console.log(spaceLength);
+//     row += Array(spaceLength).fill('');
+
+//     for (let j = 0; j < starCount; j++) {
+//       row += ['*'];
+//     }
+
+//     row += Array(spaceLength).fill('');
+
+//     starCount += 2;
+//     console.log(row);
+//     row = [];
+//   }
+
+//   //   console.log(row);
+// }
+
+// console.log(createTree(3));
+
+// uzduotis 5v2 ---------------
+
+function createTree2(height) {
+  let result = '';
+  let starCount = 1;
+  let spaceLength = height;
+
+  for (let i = 1; i < height + 1; i++) {
+    for (let k = spaceLength; k > 0; k--) {
+      result += ' ';
+    }
+
+    for (let j = 0; j < starCount; j++) {
+      result += '*';
+    }
+
+    spaceLength--;
+    starCount += 2;
+    console.log(result + '\n');
+    result = '';
+  }
+}
+
+createTree2(5);

ffa2f625c57b26e86673b344d6cfe6bd9e3d0c53
heeyniknik <nikole.official@outlook.com>
2022-07-27 18:09:11 +0300

    last

---
+++ studentai/nikoleGrablevska/lesson-5/index6.js
@@ -0,0 +1,6 @@
+
+function checkCar(carMaker, carYear) {
+    if (carMaker === "Audi" || carMaker ===  "BMW" || carMaker === "Opel" || carMaker === "VW") {
+        if (carYear >= 2010 && <= 2020)
+    }
+}

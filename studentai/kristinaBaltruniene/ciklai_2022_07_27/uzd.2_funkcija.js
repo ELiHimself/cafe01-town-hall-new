7dfe792c626b968dfc1911aabea507dac82e91fa
KristinaBal <105514019+KristinaBal@users.noreply.github.com>
2022-07-27 19:33:21 +0300

    uzd_3

--- studentai/kristinaBaltruniene/ciklai_2022_07_27/uzd.2_funkcija.js
+++ studentai/kristinaBaltruniene/ciklai_2022_07_27/uzd.2_funkcija.js
@@ -1,6 +1,6 @@
 function getLaugh(amount) {
     let result = "ha";
-    for (let i = 1; i < amount; i++) {
+    for (let i = 0; i < amount; i++) {
         result += "ha"
     }
     return result + "!"

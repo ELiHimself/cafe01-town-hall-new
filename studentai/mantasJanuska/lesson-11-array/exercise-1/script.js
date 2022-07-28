d165b252407a6ba184867b1104896cde50eac6f5
Panucio31 <m.januska1990@gmail.com>
2022-07-28 18:55:05 +0300

    exercise 1 fixed

--- studentai/mantasJanuska/lesson-11-array/exercise-1/script.js
+++ studentai/mantasJanuska/lesson-11-array/exercise-1/script.js
@@ -1,7 +1,11 @@
 let styles = ['Jazz', 'Blues'];
 
 styles.push('Rock-n-Roll');
+
 styles[1] = 'Classics';
-styles.shift();
+
+let newArr = styles.shift();
+console.log(newArr);
+
 styles.unshift('Rap', 'Reggea');
 console.log(styles);

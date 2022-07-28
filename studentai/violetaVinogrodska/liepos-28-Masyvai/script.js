b2d16704f50d834124fd8cb3b1d717c8554fab5c
Wiolla55525 <violeta.vinogrodska@gmail.com>
2022-07-28 20:34:51 +0300

    3-pries taisyma

--- studentai/violetaVinogrodska/liepos-28-Masyvai/script.js
+++ studentai/violetaVinogrodska/liepos-28-Masyvai/script.js
@@ -1,3 +1,4 @@
+//1 uzduotis
 const styles = ['Jazz', 'Blues'];
 console.log(styles);
 styles.push('Rock-n-Roll');
@@ -8,3 +9,40 @@ const newStyles = styles.shift();
 console.log(newStyles);
 styles.unshift('Rap', 'Reggae');
 console.log(styles);
+
+//2uzduotis
+let numbersArr = [11, -2, 34, 45, 19, -5, 6];
+
+function getMaxSubSum(arr) {
+    let sum = 0; 
+    for(let i = 0; i < arr.length; i++) {
+        if(arr[i] > 0) {
+            sum += arr[i];
+        }        
+    }
+    return sum;
+}
+
+console.log(getMaxSubSum(numbersArr));
+
+//3 uzduotis
+
+let randomArr = [11, 'cat', true, 4, 'mouse', 5, 9, 5, 76, false];
+
+function getArrayByData(array, dataType){
+    let newArrays = '';
+    if(typeof(dataType) === ''){
+        return dataType;
+    } else if(typeof(dataType) === Boolean){
+        return dataType;
+    } else if(typeof(dataType) === 'number'){
+        return dataType;
+    }
+
+    // for (let i = 0; i < array.length; i++){
+    //     return(array[i]);
+    // }
+}
+console.log(getArrayByData(randomArr, Boolean));
+console.log(getArrayByData(randomArr, Number));
+console.log(getArrayByData(randomArr, 'string'));

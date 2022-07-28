4374703c067049ea1f91c0a79a794edaef1609f6
kavaini <karolina.vainilkaite@gmail.com>
2022-07-28 20:24:28 +0300

    antrauzduotis

---
+++ studentai/karolinaVainilkaite/lesson-8 masyvai/antrauzduotis.js
@@ -0,0 +1,10 @@
+let numbersArr = [11, -2, 34, 45, 19, -5, 6];
+let getMaxSubSum = 0;
+
+
+for(let i = 0; i < numbersArr.length; i++){
+    if(numbersArr[i] > 0){
+        getMaxSubSum += numbersArr[i];
+    }
+}
+console.log(getMaxSubSum);

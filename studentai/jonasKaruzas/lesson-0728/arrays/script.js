57cf5e619c53daf2eba706cc0e498df21e3f0757
JonasKaruzas <jonas.karuzas@gmail.com>
2022-07-28 19:03:59 +0300

    uzduotis 2

--- studentai/jonasKaruzas/lesson-0728/arrays/script.js
+++ studentai/jonasKaruzas/lesson-0728/arrays/script.js
@@ -5,3 +5,19 @@ console.log(styles.shift());
 styles.unshift('Rap', 'Reggae');
 
 console.log(styles);
+
+// Uzduotis 2 ------------------
+
+let numbersArr = [11, -2, 34, 45, 19, -5, 6];
+
+function getMaxSubSum(arr) {
+  let result = 0;
+  for (num of arr) {
+    if (num > 0) {
+      result += num;
+    }
+  }
+  return result;
+}
+
+console.log(getMaxSubSum(numbersArr));

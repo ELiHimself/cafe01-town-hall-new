9058583a282a444c8ccb60a7d1b7b5983422c159
kavaini <karolina.vainilkaite@gmail.com>
2022-07-28 19:08:25 +0300

    pirmauzduotis

---
+++ studentai/karolinaVainilkaite/lesson-8 masyvai/pirmauzduotis.js
@@ -0,0 +1,16 @@
+let array = ["Jazz", "Blues"];
+console.log(array);
+
+array.push("Rock-n-Roll");
+console.log(array);
+
+array[1]= "Classics";
+console.log(array);
+
+console.log(array[0]);
+
+array.shift();
+console.log(array);
+
+array.unshift("Rap", "Reggae");
+console.log(array);

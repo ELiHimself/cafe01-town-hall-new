d72bd1057be447454469d59e0b5570ead0f8a164
Kristina <kristtule@gmail.com>
2022-07-28 18:59:12 +0300

    pirma uzduotis

---
+++ studentai/kristinaJachimovic/lesson-5/arrays/pirmauzduotis.js
@@ -0,0 +1,19 @@
+const arr = ['Jazz', 'Blues'];
+
+console.log(arr);
+
+arr.push('Rock-n-Roll');
+
+console.log(arr);
+
+arr[1] = 'Classics';
+
+console.log(arr);
+
+console.log(arr.shift());
+
+console.log(arr);
+
+arr.unshift('Rap', 'Reggae');
+
+console.log(arr);

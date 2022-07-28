28873bea21fdaef6f24e0efe015314d167fdc62b
EdgarasAnd <eandrejevas@yahoo.com>
2022-07-28 19:14:57 +0300

    Excercise 1 - prittier

--- studentai/edgarasAndrejevas/lesson-2022-07-28/1-script.js
+++ studentai/edgarasAndrejevas/lesson-2022-07-28/1-script.js
@@ -1,16 +1,15 @@
 const  array = ["Jazz", "Blues"];
 
 array.push("Rock-n-roll");
-
 console.log(array);
 
 array[1]="Classics";
-
 console.log(array);
 
 
 const shift = array.shift();
 console.log(shift)
 
-array.unshift("Rap", "Reggae")
-console.log(array)
+
+array.unshift("Rap", "Reggae");
+console.log(array);

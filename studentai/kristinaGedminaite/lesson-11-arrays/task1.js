654b39a09df889816cf6ec27dde95f3f311f9cdc
Kristina Gedminaite <105384146+nechbet@users.noreply.github.com>
2022-07-28 19:09:22 +0300

    task1 done

---
+++ studentai/kristinaGedminaite/lesson-11-arrays/task1.js
@@ -0,0 +1,12 @@
+let styles = ["Jazz","Blues"];
+
+styles.push("Rock'n'Roll");
+
+styles[1] = "Classics";
+
+console.log(styles.shift());
+
+styles.unshift("Rap","Reggae");
+
+console.log(styles);
+

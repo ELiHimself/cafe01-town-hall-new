626cb91695481271b9a16ba60f3a573f31c35b3d
EdgarasAnd <eandrejevas@yahoo.com>
2022-07-28 19:13:32 +0300

    First excercise Complete

---
+++ studentai/edgarasAndrejevas/lesson-2022-07-28/1-script.js
@@ -0,0 +1,16 @@
+const  array = ["Jazz", "Blues"];
+
+array.push("Rock-n-roll");
+
+console.log(array);
+
+array[1]="Classics";
+
+console.log(array);
+
+
+const shift = array.shift();
+console.log(shift)
+
+array.unshift("Rap", "Reggae")
+console.log(array)

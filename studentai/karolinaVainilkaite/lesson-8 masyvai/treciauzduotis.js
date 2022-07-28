5b7d221d8fd74e648cec6f91c0ed08a37215c512
kavaini <karolina.vainilkaite@gmail.com>
2022-07-28 20:58:25 +0300

    treciauzduotis

---
+++ studentai/karolinaVainilkaite/lesson-8 masyvai/treciauzduotis.js
@@ -0,0 +1,15 @@
+let randomArr = [11, "cat", true, "mouse", 5, 9, 5, 76, false];
+
+function getArrayByData(randomArr, dataType){
+    let arrayByData = [];
+
+for (let item of randomArr){
+    if(typeof item === dataType){
+        arrayByData.push(item);
+    
+}
+}
+return arrayByData;
+}
+console.log(getArrayByData(randomArr, "number"));
+console.log(getArrayByData(randomArr, "string"));

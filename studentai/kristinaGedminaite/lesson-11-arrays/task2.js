f63213635a8b97b3d7225199279c2d6d761393dc
Kristina Gedminaite <105384146+nechbet@users.noreply.github.com>
2022-07-28 21:17:16 +0300

    task2 sulygiuota

--- studentai/kristinaGedminaite/lesson-11-arrays/task2.js
+++ studentai/kristinaGedminaite/lesson-11-arrays/task2.js
@@ -2,10 +2,10 @@ let numbersArr = [11, -2, 34, 45, 19, -5, 6];
 
 function getMaxSubSum(arr) {
     let answer = 0;
-   for(number of arr) {
-    if (number > 0) {
-        answer += number;
-    }
+    for(number of arr) {
+        if(number > 0) {
+            answer += number;
+        }
    } 
    return answer;
 }

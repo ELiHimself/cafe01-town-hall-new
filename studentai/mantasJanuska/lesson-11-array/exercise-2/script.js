605c10a9a6dc732161a89e610d42e16a9fe041fb
Panucio31 <m.januska1990@gmail.com>
2022-07-28 19:59:50 +0300

    exercise 2 finished

--- studentai/mantasJanuska/lesson-11-array/exercise-2/script.js
+++ studentai/mantasJanuska/lesson-11-array/exercise-2/script.js
@@ -2,7 +2,7 @@ let numbersArr = [11, -2, 34, 45, 19, -5, 6];
 const posArr = numbersArr.filter(num => num > -1);
 let sum = 0;
 for (let n = 0; n < posArr.length; n++){
-    sum += posArr[n]
+    sum += posArr[n];
 }
 
 console.log(sum)

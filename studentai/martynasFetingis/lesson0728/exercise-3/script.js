2647f0d69505f2e5d297bc74090d9019b7be8981
mrtfe <105384042+mrtfe@users.noreply.github.com>
2022-07-28 20:48:46 +0300

    exercise-3

--- studentai/martynasFetingis/lesson0728/exercise-3/script.js
+++ studentai/martynasFetingis/lesson0728/exercise-3/script.js
@@ -1,14 +1,3 @@
-// let numbersArr = [11, -2, 34, 45, 19, -5, 6];
-// let sum = 0;
-
-// for (let i = 0; i < numbersArr.length; i++) {
-//   if (numbersArr[i] > 0) {
-//     sum += numbersArr[i];
-//   }
-// }
-
-// console.log(sum);
-
 let randomArr = [11, 'cat', true, 4, 'mouse', 5, 9, 5, 76, false];
 
 function getArrayByData(randomArr, dataType) {
